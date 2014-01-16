34d83d10384f51823ad1c57ea23f9aec3d5fbf1b
Randon <Randon@5519e62b-7011-5a40-965f-37cedbea8841>
2014-01-16 08:39:38 +0000

    Navigation
    git-svn-id: svn://localhost@13 5519e62b-7011-5a40-965f-37cedbea8841

--- UI/Browser.xaml.cs
+++ UI/Browser.xaml.cs
@@ -17,33 +17,63 @@ namespace NeoEdit.UI
 		{
 			InitializeComponent();
 			Directory = RecordListProvider.GetRecordList(recordUri);
+			Files.Focus();
 		}
 
-		private void Files_KeyDown(object sender, KeyEventArgs e)
+		void ClickOnItem(IRecord item)
+		{
+			if (item == null)
+				return;
+
+			var recordList = item as IRecordList;
+			if (recordList != null)
+				Directory = recordList;
+		}
+
+		void Files_KeyDown(object sender, KeyEventArgs e)
 		{
 			switch (e.Key)
 			{
 				case Key.Enter:
-					{
-						var item = Files.SelectedItem as IRecordList;
-						if (item != null)
-							Directory = item;
-					}
+					ClickOnItem(Files.SelectedItem as IRecord);
 					break;
 				case Key.Back:
 					Directory = Directory.Parent;
 					break;
 			}
+			switch (e.SystemKey)
+			{
+				case Key.D:
+					if ((Keyboard.Modifiers & (ModifierKeys.Alt | ModifierKeys.Control | ModifierKeys.Shift)) == ModifierKeys.Alt)
+					{
+						DirectoryDisplay.SelectAll();
+						DirectoryDisplay.Focus();
+					}
+					break;
+				case Key.Up:
+					if ((Keyboard.Modifiers & (ModifierKeys.Alt | ModifierKeys.Control | ModifierKeys.Shift)) == ModifierKeys.Alt)
+						Directory = Directory.Parent;
+					break;
+			}
 		}
 
-		private void DirectoryDisplay_KeyDown(object sender, KeyEventArgs e)
+		void DirectoryDisplay_KeyDown(object sender, KeyEventArgs e)
 		{
 			switch (e.Key)
 			{
+				case Key.Escape:
+					Files.Focus();
+					break;
 				case Key.Enter:
 					Directory = RecordListProvider.GetRecordList(DirectoryDisplay.Text, Directory);
+					Files.Focus();
 					break;
 			}
+		}
+
+		void Files_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+		{
+			ClickOnItem(Files.SelectedItem as IRecord);
 		}
 	}
 }
