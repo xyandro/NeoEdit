cd40a8fe7b1108de0eead1fa9c40a76fb81d57c2
rspackman <rspackman@5519e62b-7011-5a40-965f-37cedbea8841>
2014-01-17 23:50:48 +0000

    Add columns to ListView
    git-svn-id: svn://localhost@31 5519e62b-7011-5a40-965f-37cedbea8841

--- Helpers.cs
+++ Helpers.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace NeoEdit
 {
@@ -34,6 +35,29 @@ namespace NeoEdit
 				++index;
 			}
 			return -1;
+		}
+
+		public static T ParseEnum<T>(string value)
+		{
+			return (T)Enum.Parse(typeof(T), value);
+		}
+
+		public static bool IsIntegerType(this Type type)
+		{
+			switch (Type.GetTypeCode(type))
+			{
+				case TypeCode.Byte:
+				case TypeCode.SByte:
+				case TypeCode.UInt16:
+				case TypeCode.UInt32:
+				case TypeCode.UInt64:
+				case TypeCode.Int16:
+				case TypeCode.Int32:
+				case TypeCode.Int64:
+					return true;
+				default:
+					return false;
+			}
 		}
 	}
 }
---
+++ UI/RecordToStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using NeoEdit.Records;
+
+namespace NeoEdit.UI
+{
+	class RecordToStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			value = (value as Record)[Helpers.ParseEnum<Record.Property>(parameter as string)];
+			if (value == null)
+				return null;
+
+			if (value.GetType().IsIntegerType())
+				return String.Format("{0:n0}", value);
+			if (value is DateTime)
+				return ((DateTime)value).ToLocalTime();
+			return value;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return null;
+		}
+	}
+}
