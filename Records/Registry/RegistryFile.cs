8caaf9d1f62c880ee6aade8f018a2cc1d6bcd61d
Randon <Randon@5519e62b-7011-5a40-965f-37cedbea8841>
2014-02-01 18:55:50 +0000

    Fix display for various types
    git-svn-id: svn://localhost@84 5519e62b-7011-5a40-965f-37cedbea8841

--- Records/Registry/RegistryFile.cs
+++ Records/Registry/RegistryFile.cs
@@ -13,7 +13,16 @@ namespace NeoEdit.Records.Registry
 			using (var key = GetKey(Path.GetDirectoryName(FullName)))
 			{
 				this[RecordProperty.PropertyName.Type] = key.GetValueKind(Path.GetFileName(FullName)).ToString();
-				this[RecordProperty.PropertyName.Data] = key.GetValue(Path.GetFileName(FullName));
+				var value = key.GetValue(Path.GetFileName(FullName));
+				if (value is byte[])
+					value = BitConverter.ToString(value as byte[]).Replace("-", " ").ToLower();
+				else if (value is string[])
+					value = string.Join(" ", value as string[]);
+				else if (value is int)
+					value = String.Format("0x{0:x8} / {1} / {2}", value, value, (uint)(int)value);
+				else if (value is long)
+					value = String.Format("0x{0:x16} / {1} / {2}", value, value, (ulong)(long)value);
+				this[RecordProperty.PropertyName.Data] = value.ToString();
 			}
 		}
 
