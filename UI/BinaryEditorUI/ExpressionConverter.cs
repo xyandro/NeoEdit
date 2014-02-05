5b2970e35e7844f90803f6ee8be02cf86188e1ca
rspackman <rspackman@5519e62b-7011-5a40-965f-37cedbea8841>
2014-02-05 17:50:25 +0000

    Expression awesomeness
    git-svn-id: svn://localhost@134 5519e62b-7011-5a40-965f-37cedbea8841

--- UI/BinaryEditorUI/ExpressionConverter.cs
+++ UI/BinaryEditorUI/ExpressionConverter.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Data;
@@ -9,11 +11,25 @@ namespace NeoEdit.UI.BinaryEditorUI
 {
 	class ExpressionConverter : IMultiValueConverter
 	{
-		public object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)
-		{
-			var expression = parameter as string;
+		static readonly string num = @"\[(\d+)\]";
+		static readonly Regex numRE = new Regex(num);
 
-			var numRE = new Regex(@"\[(\d+)\]");
+		static readonly string term = "'((?:[^']|'')*)'";
+		static readonly Regex termOnlyRE = new Regex("^" + term + "$");
+
+		static readonly List<List<string>> binaryOperator = new List<List<string>>
+		{
+			new List<string>{ "AND" },
+			new List<string>{ "OR" },
+			new List<string>{ "==", "=i=" },
+		};
+		static readonly List<Regex> binaryOperatorREs = binaryOperator.Select(a => new Regex(String.Format(@"{0}\s*({1})\s*{0}", term, String.Join("|", a)))).ToList();
+
+		static readonly string ternaryOperator = String.Format(@"{0}\s*\?\s*{0}\s*:\s*{0}", term);
+		static readonly Regex ternaryOperatorRE = new Regex(ternaryOperator);
+
+		void ParseParameters(ref string expression, object[] value)
+		{
 			while (true)
 			{
 				var match = numRE.Match(expression);
@@ -22,33 +38,51 @@ namespace NeoEdit.UI.BinaryEditorUI
 
 				var val = value[Int32.Parse(match.Groups[1].Value)];
 				var result = val == null ? "NULL" : val.ToString();
-				expression = expression.Substring(0, match.Index) + "'" + result + "'" + expression.Substring(match.Index + match.Length);
+				expression = expression.Substring(0, match.Index) + "'" + result.Replace("'", "''") + "'" + expression.Substring(match.Index + match.Length);
 			}
+		}
 
-			var term = "'([^']*)'";
-			var termRE = new Regex("^" + term + "$");
-
-			var binaryOperation = String.Format(@"{0}\s*(AND|OR|==|=i=)\s*{0}", term);
-			var binaryOperationRE = new Regex(binaryOperation);
-
-			var ternaryOperation = String.Format(@"{0}\s*\?\s*{0}\s*:\s*{0}", term);
-			var ternaryOperationRE = new Regex(ternaryOperation);
-
-			var parens = String.Format(@"\(\s*{0}\s*\)", term);
-			var parensRE = new Regex(parens);
-			while (true)
+		string ParseParens(ref string expression)
+		{
+			bool inStr = false;
+			int expressionStart = -1;
+			int numParens = 0;
+			for (var ctr = 0; ctr < expression.Length; ctr++)
 			{
-				Match match;
-
-				match = parensRE.Match(expression);
-				if (match.Success)
+				switch (expression[ctr])
 				{
-					var result = match.Groups[1].Value;
-					expression = expression.Substring(0, match.Index) + "'" + result + "'" + expression.Substring(match.Index + match.Length);
-					continue;
+					case '\'': inStr = !inStr; break;
+					case '(':
+						if (!inStr)
+						{
+							if (expressionStart == -1)
+								expressionStart = ctr;
+							++numParens;
+						}
+						break;
+					case ')':
+						if (!inStr)
+						{
+							if (numParens <= 0)
+								throw new Exception("Invalid expression");
+							if (--numParens == 0)
+							{
+								var subQuery = expression.Substring(expressionStart + 1, ctr - expressionStart - 1);
+								expression = expression.Substring(0, expressionStart) + "#VALUE#" + expression.Substring(ctr + 1);
+								return subQuery;
+							}
+						}
+						break;
 				}
+			}
+			return null;
+		}
 
-				match = binaryOperationRE.Match(expression);
+		public bool DoBinaryOperation(ref string expression)
+		{
+			foreach (var binaryOperatorRE in binaryOperatorREs)
+			{
+				var match = binaryOperatorRE.Match(expression);
 				if (match.Success)
 				{
 					var term1 = match.Groups[1].Value;
@@ -66,48 +100,95 @@ namespace NeoEdit.UI.BinaryEditorUI
 					}
 
 					expression = expression.Substring(0, match.Index) + "'" + result + "'" + expression.Substring(match.Index + match.Length);
-					continue;
+					return true;
 				}
+			}
 
-				match = ternaryOperationRE.Match(expression);
-				if (match.Success)
+			return false;
+		}
+
+		public bool DoTernaryOperation(ref string expression)
+		{
+			var match = ternaryOperatorRE.Match(expression);
+			if (!match.Success)
+				return false;
+
+			var test = match.Groups[1].Value;
+			var result = "";
+			if (Boolean.Parse(test))
+				result = match.Groups[2].Value;
+			else
+				result = match.Groups[3].Value;
+
+			expression = expression.Substring(0, match.Index) + "'" + result + "'" + expression.Substring(match.Index + match.Length);
+			return true;
+		}
+
+		static readonly Regex boolRE = new Regex("^True|False$");
+		static readonly Regex intRE = new Regex(@"^\d+$");
+
+		object GetReturnValue(string expression, Type targetType)
+		{
+			if (boolRE.IsMatch(expression))
+			{
+				var val = Boolean.Parse(expression);
+				if (targetType == typeof(Visibility))
+					return val ? Visibility.Visible : Visibility.Collapsed;
+				return val;
+			}
+
+			if (intRE.IsMatch(expression))
+			{
+				var val = Int32.Parse(expression);
+				if (targetType == typeof(Thickness))
+					return new Thickness(val);
+				return val;
+			}
+
+			if (targetType == typeof(Brush))
+				return typeof(Brushes).GetProperty(expression).GetValue(null);
+
+			return expression;
+		}
+
+		public object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var expression = parameter as string;
+
+			ParseParameters(ref expression, value);
+
+			var expressionStack = new Stack<string>();
+
+			while (true)
+			{
+				var subExpression = ParseParens(ref expression);
+				if (subExpression != null)
 				{
-					var test = match.Groups[1].Value;
-					var result = "";
-					if (Boolean.Parse(test))
-						result = match.Groups[2].Value;
-					else
-						result = match.Groups[3].Value;
-
-					expression = expression.Substring(0, match.Index) + "'" + result + "'" + expression.Substring(match.Index + match.Length);
+					expressionStack.Push(expression);
+					expression = subExpression;
 					continue;
 				}
 
-				match = termRE.Match(expression);
+				if (DoBinaryOperation(ref expression))
+					continue;
+
+				if (DoTernaryOperation(ref expression))
+					continue;
+
+				var match = termOnlyRE.Match(expression);
 				if (!match.Success)
 					throw new Exception("Unable to calculate");
-				expression = match.Groups[1].Value;
 
-				if (new Regex("^True|False$").IsMatch(expression))
+				// If we're inside ()
+				if (expressionStack.Count != 0)
 				{
-					var val = Boolean.Parse(expression);
-					if (targetType == typeof(Visibility))
-						return val ? Visibility.Visible : Visibility.Collapsed;
-					return val;
+					expression = expressionStack.Pop().Replace("#VALUE#", expression);
+					continue;
 				}
 
-				if (new Regex(@"^\d+$").IsMatch(expression))
-				{
-					var val = Int32.Parse(expression);
-					if (targetType == typeof(Thickness))
-						return new Thickness(val);
-					return val;
-				}
+				expression = match.Groups[1].Value.Replace("''", "'");
 
-				if (targetType == typeof(Brush))
-					return typeof(Brushes).GetProperty(expression).GetValue(null);
-
-				return expression;
+				return GetReturnValue(expression, targetType);
 			}
 		}
 
