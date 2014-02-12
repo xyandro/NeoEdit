98035ef86688fdd1f7ae605a9bdf25a3638519b9
rspackman <rspackman@5519e62b-7011-5a40-965f-37cedbea8841>
2014-02-12 18:34:08 +0000

    Simplify
    git-svn-id: svn://localhost@240 5519e62b-7011-5a40-965f-37cedbea8841

--- BinaryEditorUI/Crypto.cs
+++ BinaryEditorUI/Crypto.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
-using System.Xml.Linq;
 
 namespace NeoEdit.BinaryEditorUI
 {
@@ -43,64 +40,30 @@ namespace NeoEdit.BinaryEditorUI
 			catch (Exception ex) { throw new Exception(String.Format("Decryption failed: {0}", ex.Message), ex); }
 		}
 
-		static string SerializeRSAParameters(RSAParameters rsaParameters)
-		{
-			var data = new Dictionary<string, byte[]> {
-				{ "D", rsaParameters.D },
-				{ "DP", rsaParameters.DP },
-				{ "DQ", rsaParameters.DQ },
-				{ "Exponent", rsaParameters.Exponent },
-				{ "InverseQ", rsaParameters.InverseQ },
-				{ "Modulus", rsaParameters.Modulus },
-				{ "P", rsaParameters.P },
-				{ "Q", rsaParameters.Q },
-			};
-			var xml = new XElement("RSA", data.Where(entry => entry.Value != null).Select(entry => new XElement(entry.Key, Convert.ToBase64String(entry.Value))));
-			return xml.ToString(SaveOptions.DisableFormatting);
-		}
-
-		static RSAParameters DeserializeRSAParameters(string xmlStr)
-		{
-			var xml = XElement.Parse(xmlStr);
-			return new RSAParameters
-			{
-				D = xml.Element("D") == null ? null : Convert.FromBase64String(xml.Element("D").Value),
-				DP = xml.Element("DP") == null ? null : Convert.FromBase64String(xml.Element("DP").Value),
-				DQ = xml.Element("DQ") == null ? null : Convert.FromBase64String(xml.Element("DQ").Value),
-				Exponent = xml.Element("Exponent") == null ? null : Convert.FromBase64String(xml.Element("Exponent").Value),
-				InverseQ = xml.Element("InverseQ") == null ? null : Convert.FromBase64String(xml.Element("InverseQ").Value),
-				Modulus = xml.Element("Modulus") == null ? null : Convert.FromBase64String(xml.Element("Modulus").Value),
-				P = xml.Element("P") == null ? null : Convert.FromBase64String(xml.Element("P").Value),
-				Q = xml.Element("Q") == null ? null : Convert.FromBase64String(xml.Element("Q").Value),
-			};
-		}
-
 		public static string CreateRSAPrivateKey(int keySize)
 		{
 			var rsa = new RSACryptoServiceProvider(keySize);
-			return SerializeRSAParameters(rsa.ExportParameters(true));
+			return rsa.ToXmlString(true);
 		}
 
 		public static string GetRSAPublicKey(string privKey)
 		{
-			var rsaParameters = DeserializeRSAParameters(privKey);
 			var rsa = new RSACryptoServiceProvider();
-			rsa.ImportParameters(rsaParameters);
-			rsaParameters = rsa.ExportParameters(false);
-			return SerializeRSAParameters(rsaParameters);
+			rsa.FromXmlString(privKey);
+			return rsa.ToXmlString(false);
 		}
 
 		public static byte[] EncryptRSA(byte[] data, string pubKey)
 		{
 			var rsa = new RSACryptoServiceProvider();
-			rsa.ImportParameters(DeserializeRSAParameters(pubKey));
+			rsa.FromXmlString(pubKey);
 			return rsa.Encrypt(data, false);
 		}
 
 		public static byte[] DecryptRSA(byte[] data, string privKey)
 		{
 			var rsa = new RSACryptoServiceProvider();
-			rsa.ImportParameters(DeserializeRSAParameters(privKey));
+			rsa.FromXmlString(privKey);
 			return rsa.Decrypt(data, false);
 		}
 	}
