0c3bc3fb81ba6f5414d64ef4023c6eb6e60d10b7
Randon <Randon@5519e62b-7011-5a40-965f-37cedbea8841>
2014-03-05 10:14:51 +0000

    Add global Window menu
    git-svn-id: svn://localhost@398 5519e62b-7011-5a40-965f-37cedbea8841

---
+++ GUI/Common/WindowMenu.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace NeoEdit.GUI.Common
+{
+	public partial class WindowMenu : MenuItem
+	{
+		public WindowMenu()
+		{
+			InitializeComponent();
+		}
+
+		void WindowBrowser(object sender, RoutedEventArgs e)
+		{
+			new BrowserUI.Browser();
+		}
+
+		void WindowBinaryEditor(object sender, RoutedEventArgs e)
+		{
+			new BinaryEditorUI.BinaryEditor();
+		}
+
+		void WindowTextEditor(object sender, RoutedEventArgs e)
+		{
+			new TextEditorUI.TextEditor();
+		}
+	}
+}
