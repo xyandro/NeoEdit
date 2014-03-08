bf29058bfc6b092c483c492cef057420af48b4fa
Randon <Randon@5519e62b-7011-5a40-965f-37cedbea8841>
2014-03-08 08:04:35 +0000

    Fix assembly name
    git-svn-id: svn://localhost@442 5519e62b-7011-5a40-965f-37cedbea8841

--- Browser/Properties/AssemblyInfo.Template.cs
+++ Browser/Properties/AssemblyInfo.Template.cs
@@ -16,7 +16,7 @@ $WCMODS?#error Has:// No$ local modifications
 $WCUNVER?#error Has:// No$ unversioned items
 #endif
 
-[assembly: AssemblyTitle("GUI")]
+[assembly: AssemblyTitle("Browser")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Randon Spackman")]
