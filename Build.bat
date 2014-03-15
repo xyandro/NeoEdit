3ba969b10690f4132cd7e89f49535e0e319742fb
rspackman <rspackman@5519e62b-7011-5a40-965f-37cedbea8841>
2014-03-15 02:02:20 +0000

    Delete extra files on build
    git-svn-id: svn://localhost@493 5519e62b-7011-5a40-965f-37cedbea8841

--- Build.bat
+++ Build.bat
@@ -1,13 +1,17 @@
 @ECHO OFF
 
-SET DEVENV="C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.com"
-SET SOLUTION="NeoEdit.sln"
-SET PLATFORM="Release|x64"
+SET DEVENV=C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.com
+SET SOLUTION=NeoEdit.sln
+SET CONFIGURATION=Release
+SET PLATFORM=x64
+
+SET BUILDDIR=bin\%CONFIGURATION%.%PLATFORM%
+RD /S /Q %BUILDDIR%
 
 svn up --non-interactive
-%DEVENV% %SOLUTION% /clean %PLATFORM%
-if exist Build.log del Build.log
-%DEVENV% %SOLUTION% /build %PLATFORM% /out Build.log
+"%DEVENV%" "%SOLUTION%" /clean "%CONFIGURATION%|%PLATFORM%"
+IF EXIST Build.log DEL Build.log
+"%DEVENV%" "%SOLUTION%" /build "%CONFIGURATION%|%PLATFORM%" /out Build.log
 
 IF %ERRORLEVEL% == 0 GOTO SUCCESS
 
@@ -15,5 +19,12 @@ ECHO.
 ECHO.
 ECHO Errors occured.
 PAUSE
+GOTO EXIT
 
 :SUCCESS
+DEL %BUILDDIR%\*.pdb
+DEL %BUILDDIR%\*.metagen
+DEL %BUILDDIR%\*.config
+DEL %BUILDDIR%\*.ilk
+
+:EXIT
