0cd1271e97f9a9af3d5222caf904b1c28767f58e
Randon <Randon@5519e62b-7011-5a40-965f-37cedbea8841>
2014-05-23 02:58:43 +0000

    Global find
    git-svn-id: svn://localhost@653 5519e62b-7011-5a40-965f-37cedbea8841

--- TextEditor/TextEditorWindow-Keys.cs
+++ TextEditor/TextEditorWindow-Keys.cs
@@ -28,6 +28,16 @@ namespace NeoEdit.TextEditor
 		public static RoutedCommand Command_Keys_SelectionReplace7 = new RoutedCommand();
 		public static RoutedCommand Command_Keys_SelectionReplace8 = new RoutedCommand();
 		public static RoutedCommand Command_Keys_SelectionReplace9 = new RoutedCommand();
+		public static RoutedCommand Command_Keys_GlobalFindKeys = new RoutedCommand();
+		public static RoutedCommand Command_Keys_GlobalFind1 = new RoutedCommand();
+		public static RoutedCommand Command_Keys_GlobalFind2 = new RoutedCommand();
+		public static RoutedCommand Command_Keys_GlobalFind3 = new RoutedCommand();
+		public static RoutedCommand Command_Keys_GlobalFind4 = new RoutedCommand();
+		public static RoutedCommand Command_Keys_GlobalFind5 = new RoutedCommand();
+		public static RoutedCommand Command_Keys_GlobalFind6 = new RoutedCommand();
+		public static RoutedCommand Command_Keys_GlobalFind7 = new RoutedCommand();
+		public static RoutedCommand Command_Keys_GlobalFind8 = new RoutedCommand();
+		public static RoutedCommand Command_Keys_GlobalFind9 = new RoutedCommand();
 		public static RoutedCommand Command_Keys_GlobalReplace1 = new RoutedCommand();
 		public static RoutedCommand Command_Keys_GlobalReplace2 = new RoutedCommand();
 		public static RoutedCommand Command_Keys_GlobalReplace3 = new RoutedCommand();
@@ -77,6 +87,8 @@ namespace NeoEdit.TextEditor
 				return Command_Keys_SetValues1;
 			if ((command == Command_Keys_SelectionReplace1) || (command == Command_Keys_SelectionReplace2) || (command == Command_Keys_SelectionReplace3) || (command == Command_Keys_SelectionReplace4) || (command == Command_Keys_SelectionReplace5) || (command == Command_Keys_SelectionReplace6) || (command == Command_Keys_SelectionReplace7) || (command == Command_Keys_SelectionReplace8) || (command == Command_Keys_SelectionReplace9))
 				return Command_Keys_SelectionReplace1;
+			if ((command == Command_Keys_GlobalFindKeys) || (command == Command_Keys_GlobalFind1) || (command == Command_Keys_GlobalFind2) || (command == Command_Keys_GlobalFind3) || (command == Command_Keys_GlobalFind4) || (command == Command_Keys_GlobalFind5) || (command == Command_Keys_GlobalFind6) || (command == Command_Keys_GlobalFind7) || (command == Command_Keys_GlobalFind8) || (command == Command_Keys_GlobalFind9))
+				return Command_Keys_GlobalFind1;
 			if ((command == Command_Keys_GlobalReplace1) || (command == Command_Keys_GlobalReplace2) || (command == Command_Keys_GlobalReplace3) || (command == Command_Keys_GlobalReplace4) || (command == Command_Keys_GlobalReplace5) || (command == Command_Keys_GlobalReplace6) || (command == Command_Keys_GlobalReplace7) || (command == Command_Keys_GlobalReplace8) || (command == Command_Keys_GlobalReplace9))
 				return Command_Keys_GlobalReplace1;
 			if ((command == Command_Keys_CopyKeys) || (command == Command_Keys_CopyValues1) || (command == Command_Keys_CopyValues2) || (command == Command_Keys_CopyValues3) || (command == Command_Keys_CopyValues4) || (command == Command_Keys_CopyValues5) || (command == Command_Keys_CopyValues6) || (command == Command_Keys_CopyValues7) || (command == Command_Keys_CopyValues8) || (command == Command_Keys_CopyValues9))
@@ -91,25 +103,25 @@ namespace NeoEdit.TextEditor
 
 		int GetKeysValuesIndex(ICommand command)
 		{
-			if ((command == Command_Keys_SetKeys) || (command == Command_Keys_CopyKeys) || (command == Command_Keys_HitsKeys) || (command == Command_Keys_MissesKeys))
+			if ((command == Command_Keys_SetKeys) || (command == Command_Keys_CopyKeys) || (command == Command_Keys_GlobalFindKeys) || (command == Command_Keys_HitsKeys) || (command == Command_Keys_MissesKeys))
 				return 0;
-			if ((command == Command_Keys_SetValues1) || (command == Command_Keys_SelectionReplace1) || (command == Command_Keys_GlobalReplace1) || (command == Command_Keys_CopyValues1) || (command == Command_Keys_HitsValues1) || (command == Command_Keys_MissesValues1))
+			if ((command == Command_Keys_SetValues1) || (command == Command_Keys_SelectionReplace1) || (command == Command_Keys_GlobalFind1) || (command == Command_Keys_GlobalReplace1) || (command == Command_Keys_CopyValues1) || (command == Command_Keys_HitsValues1) || (command == Command_Keys_MissesValues1))
 				return 1;
-			if ((command == Command_Keys_SetValues2) || (command == Command_Keys_SelectionReplace2) || (command == Command_Keys_GlobalReplace2) || (command == Command_Keys_CopyValues2) || (command == Command_Keys_HitsValues2) || (command == Command_Keys_MissesValues2))
+			if ((command == Command_Keys_SetValues2) || (command == Command_Keys_SelectionReplace2) || (command == Command_Keys_GlobalFind2) || (command == Command_Keys_GlobalReplace2) || (command == Command_Keys_CopyValues2) || (command == Command_Keys_HitsValues2) || (command == Command_Keys_MissesValues2))
 				return 2;
-			if ((command == Command_Keys_SetValues3) || (command == Command_Keys_SelectionReplace3) || (command == Command_Keys_GlobalReplace3) || (command == Command_Keys_CopyValues3) || (command == Command_Keys_HitsValues3) || (command == Command_Keys_MissesValues3))
+			if ((command == Command_Keys_SetValues3) || (command == Command_Keys_SelectionReplace3) || (command == Command_Keys_GlobalFind3) || (command == Command_Keys_GlobalReplace3) || (command == Command_Keys_CopyValues3) || (command == Command_Keys_HitsValues3) || (command == Command_Keys_MissesValues3))
 				return 3;
-			if ((command == Command_Keys_SetValues4) || (command == Command_Keys_SelectionReplace4) || (command == Command_Keys_GlobalReplace4) || (command == Command_Keys_CopyValues4) || (command == Command_Keys_HitsValues4) || (command == Command_Keys_MissesValues4))
+			if ((command == Command_Keys_SetValues4) || (command == Command_Keys_SelectionReplace4) || (command == Command_Keys_GlobalFind4) || (command == Command_Keys_GlobalReplace4) || (command == Command_Keys_CopyValues4) || (command == Command_Keys_HitsValues4) || (command == Command_Keys_MissesValues4))
 				return 4;
-			if ((command == Command_Keys_SetValues5) || (command == Command_Keys_SelectionReplace5) || (command == Command_Keys_GlobalReplace5) || (command == Command_Keys_CopyValues5) || (command == Command_Keys_HitsValues5) || (command == Command_Keys_MissesValues5))
+			if ((command == Command_Keys_SetValues5) || (command == Command_Keys_SelectionReplace5) || (command == Command_Keys_GlobalFind5) || (command == Command_Keys_GlobalReplace5) || (command == Command_Keys_CopyValues5) || (command == Command_Keys_HitsValues5) || (command == Command_Keys_MissesValues5))
 				return 5;
-			if ((command == Command_Keys_SetValues6) || (command == Command_Keys_SelectionReplace6) || (command == Command_Keys_GlobalReplace6) || (command == Command_Keys_CopyValues6) || (command == Command_Keys_HitsValues6) || (command == Command_Keys_MissesValues6))
+			if ((command == Command_Keys_SetValues6) || (command == Command_Keys_SelectionReplace6) || (command == Command_Keys_GlobalFind6) || (command == Command_Keys_GlobalReplace6) || (command == Command_Keys_CopyValues6) || (command == Command_Keys_HitsValues6) || (command == Command_Keys_MissesValues6))
 				return 6;
-			if ((command == Command_Keys_SetValues7) || (command == Command_Keys_SelectionReplace7) || (command == Command_Keys_GlobalReplace7) || (command == Command_Keys_CopyValues7) || (command == Command_Keys_HitsValues7) || (command == Command_Keys_MissesValues7))
+			if ((command == Command_Keys_SetValues7) || (command == Command_Keys_SelectionReplace7) || (command == Command_Keys_GlobalFind7) || (command == Command_Keys_GlobalReplace7) || (command == Command_Keys_CopyValues7) || (command == Command_Keys_HitsValues7) || (command == Command_Keys_MissesValues7))
 				return 7;
-			if ((command == Command_Keys_SetValues8) || (command == Command_Keys_SelectionReplace8) || (command == Command_Keys_GlobalReplace8) || (command == Command_Keys_CopyValues8) || (command == Command_Keys_HitsValues8) || (command == Command_Keys_MissesValues8))
+			if ((command == Command_Keys_SetValues8) || (command == Command_Keys_SelectionReplace8) || (command == Command_Keys_GlobalFind8) || (command == Command_Keys_GlobalReplace8) || (command == Command_Keys_CopyValues8) || (command == Command_Keys_HitsValues8) || (command == Command_Keys_MissesValues8))
 				return 8;
-			if ((command == Command_Keys_SetValues9) || (command == Command_Keys_SelectionReplace9) || (command == Command_Keys_GlobalReplace9) || (command == Command_Keys_CopyValues9) || (command == Command_Keys_HitsValues9) || (command == Command_Keys_MissesValues9))
+			if ((command == Command_Keys_SetValues9) || (command == Command_Keys_SelectionReplace9) || (command == Command_Keys_GlobalFind9) || (command == Command_Keys_GlobalReplace9) || (command == Command_Keys_CopyValues9) || (command == Command_Keys_HitsValues9) || (command == Command_Keys_MissesValues9))
 				return 9;
 			throw new Exception("Invalid keys/values command");
 		}
@@ -145,6 +157,23 @@ namespace NeoEdit.TextEditor
 						strs.Add(keysAndValues[index][keysHash[str]]);
 				}
 				Replace(Selections, strs, true);
+			}
+			else if (GetKeysValuesCommand(command) == Command_Keys_GlobalFind1)
+			{
+				var index = GetKeysValuesIndex(command);
+				if (keysAndValues[0].Count != keysAndValues[index].Count)
+					throw new Exception("Keys and values count must match.");
+
+				var searcher = Searcher.Create(keysAndValues[0]);
+				var ranges = new RangeList();
+				var selections = Selections;
+				if ((Selections.Count == 1) && (!Selections[0].HasSelection()))
+					selections = new RangeList { new Range(BeginOffset(), EndOffset()) };
+				foreach (var selection in selections)
+					ranges.AddRange(Data.StringMatches(searcher, selection.Start, selection.Length).Select(tuple => Range.FromIndex(tuple.Item1, tuple.Item2)));
+
+				ranges = ranges.OrderBy(range => range.Start).ToList();
+				Selections.Replace(ranges);
 			}
 			else if (GetKeysValuesCommand(command) == Command_Keys_GlobalReplace1)
 			{
