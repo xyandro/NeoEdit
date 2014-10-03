5f303ca0ce8e38a13c4d5a480e2bf59d6a685ac2
Randon <Randon@5519e62b-7011-5a40-965f-37cedbea8841>
2014-10-03 08:28:37 +0000

    More generic tab control
    git-svn-id: svn://localhost@745 5519e62b-7011-5a40-965f-37cedbea8841

--- TextEditor/TextEditorTabs.xaml.cs
+++ TextEditor/TextEditorTabs.xaml.cs
@@ -1,10 +1,29 @@
-﻿using System.Windows.Input;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Input;
+using NeoEdit.GUI.Common;
 
 namespace NeoEdit.TextEditor
 {
 	public partial class TextEditorTabs
 	{
-		public TextEditorTabs() { InitializeComponent(); }
+		[DepProp]
+		public ObservableCollection<TextEditor> TextEditors { get { return uiHelper.GetPropValue<ObservableCollection<TextEditor>>(); } set { uiHelper.SetPropValue(value); } }
+		[DepProp]
+		public TextEditor Active { get { return uiHelper.GetPropValue<TextEditor>(); } set { uiHelper.SetPropValue(value); } }
+
+		static TextEditorTabs() { UIHelper<TextEditorTabs>.Register(); }
+
+		readonly UIHelper<TextEditorTabs> uiHelper;
+		public TextEditorTabs()
+		{
+			InitializeComponent();
+			uiHelper = new UIHelper<TextEditorTabs>(this);
+		}
+	}
+
+	public class NoFocusTabControl : TabControl
+	{
 		protected override void OnKeyDown(KeyEventArgs e) { }
 	}
 }
