bf988542eaeead047df5cd8029cb75df7b0eebd6
rspackman <rspackman@5519e62b-7011-5a40-965f-37cedbea8841>
2014-10-06 22:53:38 +0000

    Add event logger (for debugging)
    git-svn-id: svn://localhost@796 5519e62b-7011-5a40-965f-37cedbea8841

---
+++ GUI/Common/EventLogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+
+namespace NeoEdit.GUI.Common
+{
+	public static class EventLogger
+	{
+		static bool enabled = false, hideCanExecute;
+		public static void LogEvents(bool _hideCanExecute = true)
+		{
+			if (enabled)
+				return;
+			hideCanExecute = _hideCanExecute;
+
+			foreach (var routedEvent in EventManager.GetRoutedEvents())
+				EventManager.RegisterClassHandler(typeof(EventLogger), routedEvent, new RoutedEventHandler(handler));
+		}
+
+		internal static void handler(object sender, RoutedEventArgs e)
+		{
+			if ((!hideCanExecute) || ((e.RoutedEvent.ToString() != "CommandManager.PreviewCanExecute") && (e.RoutedEvent.ToString() != "CommandManager.CanExecute")))
+				Console.WriteLine(e.OriginalSource + "=>" + e.RoutedEvent);
+		}
+	}
+}
