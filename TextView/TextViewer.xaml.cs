40c0dde925b51841e9557736a14c81f044b65725
Randon <Randon@5519e62b-7011-5a40-965f-37cedbea8841>
2014-11-22 13:38:43 +0000

    Add cursor, selection
    git-svn-id: svn://localhost@1078 5519e62b-7011-5a40-965f-37cedbea8841

--- TextView/TextViewer.xaml.cs
+++ TextView/TextViewer.xaml.cs
@@ -18,6 +18,14 @@ namespace NeoEdit.TextView
 		public int xScrollValue { get { return UIHelper<TextViewer>.GetPropValue<int>(this); } set { UIHelper<TextViewer>.SetPropValue(this, value); } }
 		[DepProp]
 		public int yScrollValue { get { return UIHelper<TextViewer>.GetPropValue<int>(this); } set { UIHelper<TextViewer>.SetPropValue(this, value); } }
+		[DepProp]
+		public int selCursorLine { get { return UIHelper<TextViewer>.GetPropValue<int>(this); } set { UIHelper<TextViewer>.SetPropValue(this, value); } }
+		[DepProp]
+		public int selCursorColumn { get { return UIHelper<TextViewer>.GetPropValue<int>(this); } set { UIHelper<TextViewer>.SetPropValue(this, value); } }
+		[DepProp]
+		public int selHighlightLine { get { return UIHelper<TextViewer>.GetPropValue<int>(this); } set { UIHelper<TextViewer>.SetPropValue(this, value); } }
+		[DepProp]
+		public int selHighlightColumn { get { return UIHelper<TextViewer>.GetPropValue<int>(this); } set { UIHelper<TextViewer>.SetPropValue(this, value); } }
 
 		int xScrollViewportFloor { get { return (int)Math.Floor(xScroll.ViewportSize); } }
 		int xScrollViewportCeiling { get { return (int)Math.Ceiling(xScroll.ViewportSize); } }
@@ -29,8 +37,16 @@ namespace NeoEdit.TextView
 			UIHelper<TextViewer>.Register();
 			UIHelper<TextViewer>.AddCallback(a => a.xScrollValue, (obj, o, n) => obj.renderTimer.Start());
 			UIHelper<TextViewer>.AddCallback(a => a.yScrollValue, (obj, o, n) => obj.renderTimer.Start());
+			UIHelper<TextViewer>.AddCallback(a => a.selCursorLine, (obj, o, n) => obj.renderTimer.Start());
+			UIHelper<TextViewer>.AddCallback(a => a.selCursorColumn, (obj, o, n) => obj.renderTimer.Start());
+			UIHelper<TextViewer>.AddCallback(a => a.selHighlightLine, (obj, o, n) => obj.renderTimer.Start());
+			UIHelper<TextViewer>.AddCallback(a => a.selHighlightColumn, (obj, o, n) => obj.renderTimer.Start());
 			UIHelper<TextViewer>.AddCoerce(a => a.xScrollValue, (obj, value) => (int)Math.Max(obj.xScroll.Minimum, Math.Min(obj.xScroll.Maximum, value)));
 			UIHelper<TextViewer>.AddCoerce(a => a.yScrollValue, (obj, value) => (int)Math.Max(obj.yScroll.Minimum, Math.Min(obj.yScroll.Maximum, value)));
+			UIHelper<TextViewer>.AddCoerce(a => a.selCursorLine, (obj, value) => (int)Math.Max(0, Math.Min(obj.data.NumLines, value)));
+			UIHelper<TextViewer>.AddCoerce(a => a.selCursorColumn, (obj, value) => (int)Math.Max(0, Math.Min(obj.data.NumColumns, value)));
+			UIHelper<TextViewer>.AddCoerce(a => a.selHighlightLine, (obj, value) => (int)Math.Max(0, Math.Min(obj.data.NumLines, value)));
+			UIHelper<TextViewer>.AddCoerce(a => a.selHighlightColumn, (obj, value) => (int)Math.Max(0, Math.Min(obj.data.NumColumns, value)));
 		}
 
 		RunOnceTimer renderTimer;
@@ -53,6 +69,11 @@ namespace NeoEdit.TextView
 			canvas.Render += OnCanvasRender;
 
 			MouseWheel += (s, e) => yScrollValue -= e.Delta / 40;
+
+			selCursorLine = 1;
+			selCursorColumn = 5;
+			selHighlightLine = 5;
+			selHighlightColumn = 6;
 		}
 
 		internal Label GetLabel()
@@ -70,38 +91,142 @@ namespace NeoEdit.TextView
 			ClipboardWindow.SetFiles(new List<string> { FileName }, false);
 		}
 
+		void GetSel(out int startLine, out int startColumn, out int endLine, out int endColumn)
+		{
+			if ((selCursorLine < selHighlightLine) || ((selCursorLine == selHighlightLine) && (selCursorColumn <= selHighlightColumn)))
+			{
+				startLine = selCursorLine;
+				startColumn = selCursorColumn;
+				endLine = selHighlightLine;
+				endColumn = selHighlightColumn;
+			}
+			else
+			{
+				startLine = selHighlightLine;
+				startColumn = selHighlightColumn;
+				endLine = selCursorLine;
+				endColumn = selCursorColumn;
+			}
+		}
+
 		void OnCanvasRender(object sender, DrawingContext dc)
 		{
 			if ((xScrollViewportCeiling == 0) || (yScrollViewportCeiling == 0))
 				return;
 
 			var startColumn = xScrollValue;
-			var endColumn = Math.Min(data.NumColumns, startColumn + xScrollViewportCeiling);
+			var endColumn = startColumn + xScrollViewportCeiling;
 			var numColumns = endColumn - startColumn;
 
 			var startLine = yScrollValue;
-			var endLine = Math.Min(data.NumLines, startLine + yScrollViewportCeiling);
+			var endLine = startLine + yScrollViewportCeiling;
 			var numLines = endLine - startLine;
 
-			var lines = data.GetLines(startLine, endLine);
+			var lines = data.GetLines(Math.Min(startLine, data.NumLines), Math.Min(endLine, data.NumLines));
 
-			for (var line = 0; line < numLines; ++line)
+			int selStartLine, selStartColumn, selEndLine, selEndColumn;
+			GetSel(out selStartLine, out selStartColumn, out selEndLine, out selEndColumn);
+
+			for (var line = startLine; line < endLine; ++line)
 			{
-				var str = lines[line];
+				var y = (line - startLine) * Font.lineHeight;
+
+				if ((line >= selStartLine) && (line <= selEndLine))
+				{
+					var startCol = line == selStartLine ? selStartColumn : 0;
+					var endCol = line == selEndLine ? selEndColumn : data.NumColumns;
+
+					startCol = Math.Max(0, Math.Min(xScrollViewportCeiling, startCol - xScrollValue));
+					endCol = Math.Max(0, Math.Min(xScrollViewportCeiling, endCol - xScrollValue));
+
+					if (startCol != endCol)
+						dc.DrawRectangle(Misc.selectionBrush, null, new Rect(startCol * Font.charWidth, y, (endCol - startCol) * Font.charWidth, Font.lineHeight));
+				}
+
+				if (selCursorLine == line)
+				{
+					var column = selCursorColumn - xScrollValue;
+					if ((column >= 0) && (column < xScrollViewportCeiling))
+						dc.DrawRectangle(Brushes.Black, null, new Rect(column * Font.charWidth, y, 2, Font.lineHeight));
+				}
+
+				if (line - startLine >= lines.Count)
+					continue;
+
+				var str = lines[line - startLine];
 				if (str.Length < startColumn)
 					continue;
 
-				var y = line * Font.lineHeight;
 				str = str.Substring(startColumn, Math.Min(str.Length - startColumn, numColumns));
-
 				var text = Font.GetText(str);
 				dc.DrawText(text, new Point(0, y));
 			}
 		}
 
+		void EnsureVisible()
+		{
+			yScrollValue = Math.Min(selCursorLine, Math.Max(selCursorLine - yScrollViewportFloor + 1, yScrollValue));
+			xScrollValue = Math.Min(selCursorColumn, Math.Max(selCursorColumn - xScrollViewportFloor + 1, xScrollValue));
+		}
+
+		void MoveCursor(bool shiftDown, int lineChange = 0, int columnChange = 0, bool lineRel = true, bool columnRel = true)
+		{
+			if (lineRel)
+				lineChange += selCursorLine;
+			selCursorLine = lineChange;
+
+			if (columnRel)
+				columnChange += selCursorColumn;
+			selCursorColumn = columnChange;
+
+			if (!shiftDown)
+			{
+				selHighlightLine = selCursorLine;
+				selHighlightColumn = selCursorColumn;
+			}
+
+			EnsureVisible();
+		}
+
 		internal bool HandleKey(Key key, bool shiftDown, bool controlDown)
 		{
-			return false;
+			switch (key)
+			{
+				case Key.Up: MoveCursor(shiftDown, lineChange: -1); break;
+				case Key.Down: MoveCursor(shiftDown, lineChange: 1); break;
+				case Key.Left:
+					if (controlDown)
+						MoveCursor(shiftDown, columnChange: -10);
+					else
+						MoveCursor(shiftDown, columnChange: -1);
+					break;
+				case Key.Right:
+					if (controlDown)
+						MoveCursor(shiftDown, columnChange: 20);
+					else
+						MoveCursor(shiftDown, columnChange: 1);
+					break;
+				case Key.Home:
+					if (controlDown)
+						MoveCursor(shiftDown, 0, 0, false, false);
+					else
+						MoveCursor(shiftDown, columnChange: 0, columnRel: false);
+					break;
+				case Key.End:
+					if (controlDown)
+						MoveCursor(shiftDown, data.NumLines, 0, false, false);
+					else
+						MoveCursor(shiftDown, columnChange: data.NumColumns, columnRel: false);
+					break;
+				case Key.PageUp:
+					MoveCursor(shiftDown, lineChange: -yScrollViewportFloor + 1);
+					break;
+				case Key.PageDown:
+					MoveCursor(shiftDown, lineChange: yScrollViewportFloor - 1);
+					break;
+				default: return false;
+			}
+			return true;
 		}
 
 		protected override void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e)
@@ -122,14 +247,14 @@ namespace NeoEdit.TextView
 
 			xScroll.ViewportSize = canvas.ActualWidth / Font.charWidth;
 			xScroll.Minimum = 0;
-			xScroll.Maximum = data.NumColumns - xScrollViewportFloor;
+			xScroll.Maximum = data.NumColumns - xScrollViewportFloor + 1;
 			xScroll.SmallChange = 1;
 			xScroll.LargeChange = Math.Max(0, xScroll.ViewportSize - 1);
 			xScrollValue = xScrollValue;
 
 			yScroll.ViewportSize = canvas.ActualHeight / Font.lineHeight;
 			yScroll.Minimum = 0;
-			yScroll.Maximum = data.NumLines - yScrollViewportFloor;
+			yScroll.Maximum = data.NumLines - yScrollViewportFloor + 1;
 			yScroll.SmallChange = 1;
 			yScroll.LargeChange = Math.Max(0, yScroll.ViewportSize - 1);
 			yScrollValue = yScrollValue;
