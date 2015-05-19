30790c3d47a443044594279506183b6b424c9d63
Randon <Randon@5519e62b-7011-5a40-965f-37cedbea8841>
2015-05-19 01:45:21 +0000

    Improve gesture text
    git-svn-id: svn://localhost@1290 5519e62b-7011-5a40-965f-37cedbea8841

--- GUI/Common/NECommand.cs
+++ GUI/Common/NECommand.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using System.Windows;
@@ -15,12 +14,42 @@ namespace NeoEdit.GUI.Common
 		public Key Key { get; private set; }
 		public ModifierKeys Modifiers { get; private set; }
 		public bool Primary { get; private set; }
+		public string GestureText { get; private set; }
 
 		public KeyGestureAttribute(Key key, ModifierKeys modifiers = ModifierKeys.None, bool primary = true)
 		{
 			Key = key;
 			Modifiers = modifiers;
 			Primary = primary;
+			var mods = new List<string>();
+			if ((modifiers & ModifierKeys.Control) != 0)
+				mods.Add("Ctrl");
+			if ((modifiers & ModifierKeys.Windows) != 0)
+				mods.Add("Win");
+			if ((modifiers & ModifierKeys.Alt) != 0)
+				mods.Add("Alt");
+			if ((modifiers & ModifierKeys.Shift) != 0)
+				mods.Add("Shift");
+			switch (key)
+			{
+				case Key.D0:
+				case Key.D1:
+				case Key.D2:
+				case Key.D3:
+				case Key.D4:
+				case Key.D5:
+				case Key.D6:
+				case Key.D7:
+				case Key.D8:
+				case Key.D9: mods.Add(key.ToString().Substring(1)); break;
+				case Key.OemPlus: mods.Add("+"); break;
+				case Key.OemMinus: mods.Add("-"); break;
+				case Key.OemPeriod: mods.Add("."); break;
+				case Key.OemOpenBrackets: mods.Add("["); break;
+				case Key.OemCloseBrackets: mods.Add("]"); break;
+				default: mods.Add(key.ToString()); break;
+			}
+			GestureText = String.Join("+", mods);
 		}
 	}
 
@@ -41,7 +70,7 @@ namespace NeoEdit.GUI.Common
 				foreach (var key in keyGestureAttrs)
 				{
 					var gesture = new KeyGesture(key.Key, key.Modifiers);
-					KeyGestures.Add(new Tuple<KeyGesture, string>(gesture, gesture.GetDisplayStringForCulture(CultureInfo.CurrentCulture)));
+					KeyGestures.Add(new Tuple<KeyGesture, string>(gesture, key.GestureText));
 				}
 				if (KeyGestures.Any())
 					InputGestureText = KeyGestures.First().Item2;
