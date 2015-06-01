8ddcd879cb6b3beda7a354955e0189bccde9458c
RSpackma <RSpackma@5519e62b-7011-5a40-965f-37cedbea8841>
2015-06-01 18:44:38 +0000

    Refactor XMLConverter, add unit test
    git-svn-id: svn://localhost@1360 5519e62b-7011-5a40-965f-37cedbea8841

--- Common/Transform/XMLConverter.cs
+++ Common/Transform/XMLConverter.cs
@@ -11,37 +11,109 @@ using System.Xml.Linq;
 
 namespace NeoEdit.Common.Transform
 {
-	public class XMLConverter
+	public static class XMLConverter
 	{
-		XMLConverter() { }
+		const string typeTag = "Type";
+		const string guidTag = "GUID";
+		const string listItemTag = "Item";
+		const string referenceType = "Reference";
+		const string nullType = "NULL";
 
-		public static XElement ToXML<T>(T obj, string name = null)
+		public static XElement ToXML(object obj)
 		{
-			if (name == null)
-				name = typeof(T).Name;
-			var converter = new XMLConverter();
-			var xml = converter.rToXML(obj, name, null, false) as XElement;
-			xml.Add(new XElement("NameMapping",
-				new XElement("FieldNames", converter.fieldNames.Select(pair => new XElement("FieldName", new XAttribute("Key", pair.Key), new XAttribute("Value", pair.Value)))),
-				new XElement("TypeNames", converter.typeNames.Select(pair => new XElement("TypeName", new XAttribute("Key", pair.Key), new XAttribute("Value", pair.Value))))
-			));
-			return xml;
+			var name = obj == null ? "Root" : obj.GetType().Name;
+			return rToXML(name, obj, null, new Dictionary<object, XElement>()) as XElement;
 		}
 
-		public static void Save<T>(T obj, string fileName, string name = null)
+		public static void Save(object obj, string fileName)
 		{
-			ToXML(obj, name).Save(fileName);
+			ToXML(obj).Save(fileName);
+		}
+
+		static string EscapeField(MemberInfo field, HashSet<string> found)
+		{
+			var name = field.Name;
+			if ((field.GetCustomAttribute(typeof(CompilerGeneratedAttribute)) != null) && (name.Contains("BackingField")))
+			{
+				var start = name.IndexOf('<');
+				var end = name.IndexOf('>');
+				if ((start != -1) && (end != -1))
+					name = name.Substring(start + 1, end - start - 1);
+			}
+			name = Regex.Replace(name, @"\W+", match => "_");
+			while (name.StartsWith("_"))
+				name = name.Substring(1);
+
+			var reserved = new HashSet<string> { "Type", "GUID", "Reference" };
+
+			string useName;
+			for (var ctr = 0; ; ++ctr)
+			{
+				useName = name + (ctr == 0 ? "" : ctr.ToString());
+				if ((!String.IsNullOrWhiteSpace(useName)) && (!reserved.Contains(useName)) && (!found.Contains(useName)))
+					break;
+			}
+			found.Add(useName);
+			return useName;
+		}
+
+		public static XObject rToXML(string name, object obj, Type expectedType, Dictionary<object, XElement> references)
+		{
+			if (obj == null)
+				return new XElement(name, new XAttribute(typeTag, nullType));
+
+			var type = obj.GetType();
+			if ((type.IsPrimitive) || (type.IsEnum) || (type == typeof(string)))
+				return new XAttribute(name, obj.ToString());
+
+			if (references.ContainsKey(obj))
+			{
+				var reference = references[obj];
+				if (reference.Attribute(guidTag) == null)
+					reference.Add(new XAttribute(guidTag, Guid.NewGuid().ToString()));
+				var guid = reference.Attribute(guidTag).Value;
+				return new XElement(name, new XAttribute(typeTag, referenceType), new XAttribute(guidTag, guid));
+			}
+
+			var xml = references[obj] = new XElement(name);
+			if (type != expectedType)
+				xml.Add(new XAttribute(typeTag, type.FullName));
+
+			if (obj is Regex)
+			{
+				var regex = obj as Regex;
+				xml.Add(regex.ToString(), new XAttribute("Options", regex.Options));
+			}
+			else if (obj is IList)
+			{
+				var list = obj as IList;
+				var listType = type.GetGenericArguments()[0];
+				if (list != null)
+					foreach (var item in list)
+						xml.Add(rToXML(listItemTag, item, listType, references));
+			}
+			else
+			{
+				var found = new HashSet<string>();
+				var fields = type.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+				foreach (var field in fields)
+				{
+					var fieldName = EscapeField(field, found);
+
+					var value = field.GetValue(obj);
+					if (value == null)
+						continue;
+
+					xml.Add(rToXML(fieldName, value, field.FieldType, references));
+				}
+			}
+
+			return xml;
 		}
 
 		public static T FromXML<T>(XElement xml)
 		{
-			var nameMapping = xml.Element("NameMapping");
-			var converter = new XMLConverter
-			{
-				fieldNames = nameMapping.Element("FieldNames").Elements().ToDictionary(element => element.Attribute("Key").Value, element => element.Attribute("Value").Value),
-				typeNames = nameMapping.Element("TypeNames").Elements().ToDictionary(element => element.Attribute("Value").Value, element => element.Attribute("Key").Value),
-			};
-			return (T)converter.rFromXML(xml, typeof(T));
+			return (T)(rFromXML(xml, typeof(T), new Dictionary<string, object>()));
 		}
 
 		public static T Load<T>(string fileName)
@@ -49,293 +121,58 @@ namespace NeoEdit.Common.Transform
 			return FromXML<T>(XElement.Load(fileName));
 		}
 
-		Dictionary<object, XElement> toXMLReferences = new Dictionary<object, XElement>();
-		object rToXML(object obj, string name, Type expectedType, bool rawToAttr)
+		public static OutputType Next<InputType, OutputType>(this InputType input, Func<InputType, OutputType> func)
 		{
-			var type = expectedType;
-			if (obj != null)
-				type = obj.GetType();
+			return input == null ? default(OutputType) : func(input);
+		}
 
-			var xml = new XElement(name);
-			if (type != expectedType)
-				if ((expectedType == null) || (!expectedType.IsGenericType) || (expectedType.GetGenericTypeDefinition() != typeof(Nullable<>)) || (expectedType.GetGenericArguments()[0] != type))
-					xml.Add(new XAttribute("Type", EscapeType(type)));
+		public static object rFromXML(XObject xObj, Type type, Dictionary<string, object> references)
+		{
+			if (xObj is XAttribute)
+				return TypeDescriptor.GetConverter(type).ConvertFrom((xObj as XAttribute).Value);
 
-			if (ToRaw(type, obj, xml))
+			var xml = xObj as XElement;
+			var typeValue = xml.Attribute(typeTag).Next(a => a.Value);
+			if (typeValue == referenceType)
+				return references[xml.Attribute(guidTag).Value];
+			if (typeValue == nullType)
+				return null;
+
+			type = typeValue.Next(a => AppDomain.CurrentDomain.GetAssemblies().Select(assembly => assembly.GetType(a)).First(val => val != null)) ?? type;
+			var obj = FormatterServices.GetUninitializedObject(type);
+			if (obj is IList)
+				obj = Activator.CreateInstance(type);
+			else if (obj is Regex)
+				obj = new Regex(xml.Value, (RegexOptions)Enum.Parse(typeof(RegexOptions), xml.Attribute("Options").Value));
+
+			var guid = xml.Attribute(guidTag).Next(a => a.Value);
+			if (guid != null)
+				references[guid] = obj;
+
+			if (obj is Regex)
+			{ }
+			else if (obj is IList)
 			{
-				if ((rawToAttr) && (!xml.IsEmpty) && (!xml.HasElements) && (!xml.HasAttributes))
-					return new XAttribute(xml.Name, xml.Value);
+				var list = obj as IList;
 
-				return xml;
+				var listType = type.GetGenericArguments()[0];
+				foreach (var element in xml.Elements(listItemTag))
+					list.Add(rFromXML(element, listType, references));
 			}
-
-			if (obj == null)
-				xml.Add(new XAttribute("Type", "null"));
 			else
 			{
-				if (toXMLReferences.ContainsKey(obj))
-				{
-					if (toXMLReferences[obj].Attribute("GUID") == null)
-						toXMLReferences[obj].Add(new XAttribute("GUID", Guid.NewGuid().ToString()));
-					xml.RemoveAttributes();
-					xml.Add(new XAttribute("Reference", toXMLReferences[obj].Attribute("GUID").Value));
-					return xml;
-				}
-				toXMLReferences[obj] = xml;
-
+				var found = new HashSet<string>();
 				var fields = type.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
 				foreach (var field in fields)
 				{
-					var value = field.GetValue(obj);
+					var fieldName = EscapeField(field, found);
+					var value = xml.Element(fieldName) as XObject ?? xml.Attribute(fieldName) as XObject;
 					if (value != null)
-						xml.Add(rToXML(value, EscapeField(field), field.FieldType, true));
+						field.SetValue(obj, rFromXML(value, field.FieldType, references));
 				}
 			}
 
-			return xml;
-		}
-
-		Dictionary<string, object> fromXMLReferences = new Dictionary<string, object>();
-		object rFromXML(XElement xml, Type type)
-		{
-			var reference = xml.Attribute("Reference") == null ? null : xml.Attribute("Reference").Value;
-			if (!String.IsNullOrEmpty(reference))
-				return fromXMLReferences[reference];
-
-			if (xml.Attribute("Type") != null)
-			{
-				var typeName = xml.Attribute("Type").Value;
-				if (typeName == "null")
-					return null;
-				type = AppDomain.CurrentDomain.GetAssemblies().Select(assembly => assembly.GetType(typeNames[typeName])).First(val => val != null);
-			}
-
-			object raw;
-			if (FromRaw(type, xml, out raw))
-				return raw;
-
-			var obj = FormatterServices.GetUninitializedObject(type);
-
-			var guid = xml.Attribute("GUID") == null ? null : xml.Attribute("GUID").Value;
-			if (!String.IsNullOrEmpty(guid))
-				fromXMLReferences[guid] = obj;
-
-			var fields = type.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
-			foreach (var field in fields)
-			{
-				if (!fieldNames.ContainsKey(field.Name))
-					continue;
-				var name = fieldNames[field.Name];
-				if (xml.Element(name) != null)
-					field.SetValue(obj, rFromXML(xml.Element(name), field.FieldType));
-				else if ((xml.Attribute(name) != null) && (FromRaw(field.FieldType, xml.Attribute(name).Value, out raw)))
-					field.SetValue(obj, raw);
-			}
 			return obj;
-		}
-
-		string GetUniqueName(string name, HashSet<string> used)
-		{
-			var reserved = new HashSet<string> { "Type", "GUID", "Reference" };
-			var ctr = 0;
-			while (true)
-			{
-				var useName = name + (++ctr == 1 ? "" : ctr.ToString());
-				if ((String.IsNullOrEmpty(useName)) || (reserved.Contains(useName)) || (used.Contains(useName)))
-					continue;
-				return useName;
-			}
-		}
-
-		Dictionary<string, string> fieldNames = new Dictionary<string, string>();
-		string EscapeField(MemberInfo field)
-		{
-			if (!fieldNames.ContainsKey(field.Name))
-			{
-				var name = field.Name;
-				if ((field.GetCustomAttribute(typeof(CompilerGeneratedAttribute)) != null) && (name.Contains("BackingField")))
-				{
-					var start = name.IndexOf('<');
-					var end = name.IndexOf('>');
-					if ((start != -1) && (end != -1))
-						name = name.Substring(start + 1, end - start - 1);
-				}
-				name = Regex.Replace(name, @"\W", match => "_");
-				while (name.StartsWith("_"))
-					name = name.Substring(1);
-				name = GetUniqueName(name, new HashSet<string>(fieldNames.Values));
-				fieldNames[field.Name] = name;
-			}
-
-			return fieldNames[field.Name];
-		}
-
-		Dictionary<string, string> typeNames = new Dictionary<string, string>();
-		string EscapeType(Type type)
-		{
-			if (!typeNames.ContainsKey(type.FullName))
-			{
-				var name = type.Name;
-				if ((type.IsGenericType) && (type.GetGenericTypeDefinition() == typeof(List<>)))
-					name = "List-" + EscapeType(type.GetGenericArguments()[0]);
-				else if ((type.IsGenericType) && (type.GetGenericTypeDefinition() == typeof(Dictionary<,>)))
-					name = "Dictionary-" + EscapeType(type.GetGenericArguments()[0]) + "-" + EscapeType(type.GetGenericArguments()[1]);
-				name = GetUniqueName(name, new HashSet<string>(typeNames.Values));
-				typeNames[type.FullName] = name;
-			}
-
-			return typeNames[type.FullName];
-		}
-
-		bool ToRaw(Type type, object obj, XElement xml)
-		{
-			if ((type.IsPrimitive) || (!type.IsClass) || (type == typeof(string)))
-			{
-				xml.Add(obj);
-				return true;
-			}
-
-			if (type == typeof(Regex))
-			{
-				var regex = obj as Regex;
-				if (regex != null)
-					xml.Add(new XElement("Pattern", regex.ToString()), new XElement("Options", regex.Options));
-				return true;
-			}
-
-			if (type.IsArray)
-			{
-				var arrayType = type.GetElementType();
-				if (arrayType == typeof(byte))
-				{
-					xml.Add(Coder.BytesToString(obj as byte[], Coder.CodePage.Hex));
-					return true;
-				}
-
-				var array = obj as Array;
-				if (array != null)
-				{
-					foreach (var item in array)
-						xml.Add(rToXML(item, "Item", arrayType, false));
-				}
-
-				return true;
-			}
-
-			if ((type.IsGenericType) && (type.GetGenericTypeDefinition() == typeof(List<>)))
-			{
-				var list = obj as IList;
-				if (list != null)
-				{
-					var listType = type.GetGenericArguments()[0];
-					foreach (var item in list)
-						xml.Add(rToXML(item, "Item", listType, false));
-				}
-
-				return true;
-			}
-
-			if ((type.IsGenericType) && (type.GetGenericTypeDefinition() == typeof(Dictionary<,>)))
-			{
-				var dictionary = obj as IDictionary;
-				if (dictionary != null)
-				{
-					var keyType = type.GetGenericArguments()[0];
-					var valueType = type.GetGenericArguments()[1];
-					foreach (DictionaryEntry item in dictionary)
-						xml.Add(new XElement("Item", rToXML(item.Key, "Key", keyType, false), rToXML(item.Value, "Value", valueType, false)));
-				}
-
-				return true;
-			}
-
-			return false;
-		}
-
-		bool FromRaw(Type type, string value, out object raw)
-		{
-			raw = null;
-			if ((type.IsPrimitive) || (!type.IsClass) || (type == typeof(string)))
-			{
-				if (value == null)
-				{
-					if (type.IsValueType)
-						raw = Activator.CreateInstance(type);
-					return true;
-				}
-				raw = TypeDescriptor.GetConverter(type).ConvertFrom(value);
-				return true;
-			}
-
-			if ((type.IsArray) && (type.GetElementType() == typeof(byte)))
-			{
-				raw = Coder.StringToBytes(value, Coder.CodePage.Hex);
-				return true;
-			}
-
-			return false;
-		}
-
-		bool FromRaw(Type type, XElement xml, out object raw)
-		{
-			if (FromRaw(type, xml.IsEmpty ? null : xml.Value, out raw))
-				return true;
-
-			if (type == typeof(Regex))
-			{
-				if (!xml.IsEmpty)
-					raw = new Regex(xml.Element("Pattern").Value, (RegexOptions)Enum.Parse(typeof(RegexOptions), xml.Element("Options").Value));
-				return true;
-			}
-
-			if (type.IsArray)
-			{
-				if (!xml.IsEmpty)
-				{
-					var elements = xml.Elements("Item").ToList();
-					raw = type.GetConstructor(new Type[] { typeof(int) }).Invoke(new object[] { elements.Count });
-					var array = raw as Array;
-
-					var arrayType = type.GetElementType();
-					for (var ctr = 0; ctr < elements.Count; ++ctr)
-						array.SetValue(rFromXML(elements[ctr], arrayType), ctr);
-				}
-
-				return true;
-			}
-
-			if ((type.IsGenericType) && (type.GetGenericTypeDefinition() == typeof(List<>)))
-			{
-				if (!xml.IsEmpty)
-				{
-					raw = type.GetConstructor(Type.EmptyTypes).Invoke(null);
-					var list = raw as IList;
-
-					var listType = type.GetGenericArguments()[0];
-					foreach (var element in xml.Elements("Item"))
-						list.Add(rFromXML(element, listType));
-				}
-
-				return true;
-			}
-
-			if ((type.IsGenericType) && (type.GetGenericTypeDefinition() == typeof(Dictionary<,>)))
-			{
-				if (!xml.IsEmpty)
-				{
-					raw = type.GetConstructor(Type.EmptyTypes).Invoke(null);
-					var dictionary = raw as IDictionary;
-
-					var keyType = type.GetGenericArguments()[0];
-					var valueType = type.GetGenericArguments()[1];
-					foreach (var element in xml.Elements("Item"))
-						dictionary.Add(rFromXML(element.Element("Key"), keyType), rFromXML(element.Element("Value"), valueType));
-				}
-
-				return true;
-			}
-
-			return false;
 		}
 	}
 }
---
+++ Common/UnitTest/XMLConverterTest.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeoEdit.Common.Transform;
+
+namespace NeoEdit.Common.UnitTest
+{
+	public partial class UnitTest
+	{
+		class XMLConverterGenericBase<T>
+		{
+			public T Val1, Val2;
+		}
+
+		class XMLConverterGenericDerived<T> : XMLConverterGenericBase<T> { }
+
+		class XMLConverterBase
+		{
+			public int Int1, Int2;
+			public int? IntNullable1, IntNullable2;
+			public Regex Regex1, Regex2;
+			public XMLConverterBase SubClass1, SubClass2, SubClass3;
+		}
+
+		class XMLConverterDerived : XMLConverterBase
+		{
+			public List<XMLConverterBase> Children { get; set; }
+			public XMLConverterGenericBase<float?> Generic { get; set; }
+			public XMLConverterDerived Self { get; set; }
+		}
+
+		[TestMethod]
+		public void XMLConverterTest()
+		{
+			var test1 = new XMLConverterDerived
+			{
+				Int1 = 1,
+				Int2 = default(int),
+				IntNullable1 = 2,
+				IntNullable2 = null,
+				Regex1 = new Regex("Pattern", RegexOptions.Compiled | RegexOptions.IgnoreCase),
+				Regex2 = null,
+				SubClass1 = new XMLConverterBase { Int1 = 3 },
+				SubClass2 = new XMLConverterDerived { Int1 = 4 },
+				SubClass3 = null,
+				Children = new List<XMLConverterBase> { new XMLConverterBase { Int1 = 5 }, new XMLConverterDerived { Int1 = 6 }, null },
+				Generic = new XMLConverterGenericDerived<float?> { Val1 = 5.5F, Val2 = null }
+			};
+			test1.Self = test1;
+			var xml = XMLConverter.ToXML(test1);
+			var test2 = XMLConverter.FromXML<XMLConverterBase>(xml) as XMLConverterDerived;
+
+			Assert.IsNotNull(test1);
+			Assert.IsNotNull(test2);
+
+			Assert.AreEqual(test1.Int1, test2.Int1);
+			Assert.AreEqual(test1.Int2, test2.Int2);
+			Assert.AreEqual(test1.IntNullable1, test2.IntNullable1);
+			Assert.AreEqual(test1.IntNullable2, test2.IntNullable2);
+			Assert.AreEqual(test1.Regex1.ToString(), test2.Regex1.ToString());
+			Assert.AreEqual(test1.Regex1.Options, test2.Regex1.Options);
+			Assert.AreEqual(test1.Regex2, test2.Regex2);
+			Assert.AreEqual(test1.SubClass1.GetType(), test2.SubClass1.GetType());
+			Assert.AreEqual(test1.SubClass2.GetType(), test2.SubClass2.GetType());
+			Assert.AreEqual(test1.SubClass1.Int1, test2.SubClass1.Int1);
+			Assert.AreEqual(test1.SubClass2.Int1, test2.SubClass2.Int1);
+			Assert.AreEqual(test1.SubClass3, test2.SubClass3);
+			Assert.AreEqual(test1.Children[0].GetType(), test2.Children[0].GetType());
+			Assert.AreEqual(test1.Children[1].GetType(), test2.Children[1].GetType());
+			Assert.AreEqual(test1.Children.Count, test2.Children.Count);
+			Assert.AreEqual(test1.Children[0].Int1, test2.Children[0].Int1);
+			Assert.AreEqual(test1.Children[1].Int1, test2.Children[1].Int1);
+			Assert.AreEqual(test1.Children[2], test2.Children[2]);
+			Assert.AreEqual(test1.Generic.GetType(), test2.Generic.GetType());
+			Assert.AreEqual(test1.Generic.Val1, test2.Generic.Val1);
+			Assert.AreEqual(test1.Generic.Val2, test2.Generic.Val2);
+			Assert.IsTrue(test1 == test1.Self);
+			Assert.IsTrue(test2 == test2.Self);
+		}
+	}
+}
