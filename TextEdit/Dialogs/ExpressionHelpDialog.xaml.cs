5dbef535bdc40f519294babefb911d8aea982021
RSpackma <RSpackma@5519e62b-7011-5a40-965f-37cedbea8841>
2015-06-10 21:52:42 +0000

    Refactor regex help
    git-svn-id: svn://localhost@1431 5519e62b-7011-5a40-965f-37cedbea8841

--- TextEdit/Dialogs/ExpressionHelpDialog.xaml.cs
+++ TextEdit/Dialogs/ExpressionHelpDialog.xaml.cs
@@ -1,24 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Windows;
+﻿using System.Windows;
 
 namespace NeoEdit.TextEdit.Dialogs
 {
-	class HelpItems
-	{
-		public string Name { get; set; }
-		public List<HelpItem> Items { get; set; }
-		public int Columns { get; set; }
-		public int NameWidth { get; set; }
-		public int DescWidth { get; set; }
-		public HelpItems() { Items = new List<HelpItem>(); }
-	}
-
-	class HelpItem
-	{
-		public string Name { get; set; }
-		public string Description { get; set; }
-	}
-
 	partial class ExpressionHelpDialog
 	{
 		static ExpressionHelpDialog singleton = null;
---
+++ TextEdit/Dialogs/HelpItems.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace NeoEdit.TextEdit.Dialogs
+{
+	class HelpItems
+	{
+		public string Name { get; set; }
+		public List<HelpItem> Items { get; set; }
+		public int Columns { get; set; }
+		public int NameWidth { get; set; }
+		public int DescWidth { get; set; }
+		public HelpItems() { Items = new List<HelpItem>(); }
+	}
+
+	class HelpItem
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+	}
+}
