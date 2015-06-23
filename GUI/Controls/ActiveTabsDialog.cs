fcb3be0d0d3bdec1e329ffa6cb2e0313175d4fc2
RSpackma <RSpackma@5519e62b-7011-5a40-965f-37cedbea8841>
2015-06-23 15:17:05 +0000

    Don't have active checkboxes; just count selection
    git-svn-id: svn://localhost@1489 5519e62b-7011-5a40-965f-37cedbea8841

--- GUI/Controls/ActiveTabsDialog.cs
+++ GUI/Controls/ActiveTabsDialog.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Linq;
@@ -6,66 +7,67 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Data;
-using System.Windows.Input;
 
 namespace NeoEdit.GUI.Controls
 {
 	class ActiveTabsDialog<ItemType> : ModalDialog where ItemType : FrameworkElement
 	{
+		readonly List<Tabs<ItemType>.ItemData> original;
+		readonly ObservableCollection<Tabs<ItemType>.ItemData> items;
 		public ActiveTabsDialog(ObservableCollection<Tabs<ItemType>.ItemData> items, string tabLabelPath)
 		{
-			Setup(items, tabLabelPath);
+			this.items = items;
+			original = items.Where(item => item.Active).ToList();
+			Setup(tabLabelPath);
 		}
 
-		void Setup(ObservableCollection<Tabs<ItemType>.ItemData> items, string tabLabelPath)
+		void Setup(string tabLabelPath)
 		{
 			Title = "Active Tabs";
 			ListView listView;
 			var stackPanel = new StackPanel { Margin = new Thickness(10) };
 			{
-				listView = new ListView { ItemsSource = items, Height = 400 };
+				listView = new ListView { ItemsSource = items, Height = 400, SelectionMode = SelectionMode.Extended };
 				{
 					var gridView = new GridView();
-					{
-						var column = new GridViewColumn { Header = "Active" };
-						{
-							var dataTemplate = new DataTemplate();
-							{
-								var checkBox = new FrameworkElementFactory(typeof(CheckBox));
-								checkBox.SetBinding(CheckBox.IsCheckedProperty, new Binding(UIHelper<Tabs<ItemType>.ItemData>.GetProperty(a => a.Active).Name));
-								dataTemplate.VisualTree = checkBox;
-							}
-							column.CellTemplate = dataTemplate;
-						}
-						gridView.Columns.Add(column);
-					}
 					gridView.Columns.Add(new GridViewColumn { Header = "Label", DisplayMemberBinding = new Binding(UIHelper<Tabs<ItemType>.ItemData>.GetProperty(a => a.Item).Name) { Converter = new GetLabelConverter(tabLabelPath) }, Width = 500 });
 					listView.View = gridView;
 				}
-				listView.PreviewKeyDown += (s, e) =>
-				{
-					if (e.Key == Key.Space)
-					{
-						var selected = listView.SelectedItems.Cast<Tabs<ItemType>.ItemData>().ToList();
-						var status = !selected.All(item => item.Active == true);
-						selected.ForEach(item => item.Active = status);
-						e.Handled = true;
-					}
-				};
+				listView.SelectionChanged += (s, e) => SyncItems(listView.SelectedItems.Cast<Tabs<ItemType>.ItemData>());
 				stackPanel.Children.Add(listView);
 			}
 			{
 				var uniformGrid = new UniformGrid { HorizontalAlignment = HorizontalAlignment.Right, Rows = 1, Margin = new Thickness(0, 10, 0, 0) };
 				{
-					var okButton = new Button { IsDefault = true, IsCancel = true, Content = "Ok", Padding = new Thickness(10, 1, 10, 1) };
+					var okButton = new Button { IsDefault = true, Content = "Ok", Padding = new Thickness(10, 1, 10, 1) };
 					okButton.Click += (s, e) => DialogResult = true;
 					uniformGrid.Children.Add(okButton);
+				}
+				{
+					var cancelButton = new Button { IsCancel = true, Content = "Cancel", Padding = new Thickness(10, 1, 10, 1) };
+					cancelButton.Click += (s, e) =>
+					{
+						SyncItems(original);
+						DialogResult = false;
+					};
+					uniformGrid.Children.Add(cancelButton);
 				}
 				stackPanel.Children.Add(uniformGrid);
 			}
 			Content = stackPanel;
 			listView.Focus();
 			SizeToContent = SizeToContent.WidthAndHeight;
+
+			listView.SelectedItems.Clear();
+			foreach (var item in original)
+				listView.SelectedItems.Add(item);
+		}
+
+		void SyncItems(IEnumerable<Tabs<ItemType>.ItemData> active)
+		{
+			var activeHash = new HashSet<Tabs<ItemType>.ItemData>(active);
+			foreach (var item in items)
+				item.Active = activeHash.Contains(item);
 		}
 
 		public static void Run(Window parent, ObservableCollection<Tabs<ItemType>.ItemData> items, string tabLabelPath)
