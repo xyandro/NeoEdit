637bc213c413385f873f3254e9630d31cc808628
rspackman <randon_s@yahoo.com>
2015-09-16 09:10:23 -0600

    Can connect to MSSQL; encrypt ConnectionString while in memory

--- TextEdit/DBConnectInfo.cs
+++ TextEdit/DBConnectInfo.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Data.Common;
+using System.Data.SqlClient;
 using MySql.Data.MySqlClient;
+using NeoEdit.Common.Transform;
 
 namespace NeoEdit.TextEdit
 {
@@ -9,33 +11,39 @@ namespace NeoEdit.TextEdit
 		public enum DBType
 		{
 			MySQL,
+			MSSQL,
 		}
 
 		public string Name { get; set; }
 		public DBType Type { get; set; }
-		public string ConnectionString { get; set; }
+		string connectionString;
+		const string encryptionKey = "<RSAKeyValue><Modulus>t5X3OepqSDm+YjqiW574OcoYoxLkt4tcEQB7B3+kR9rEVUSUocnQ2pA/rpz91u6crGORz5sUz8rp9NvWhrDeuN/KqZXo8cWGr2l7t6taiAxw+PwQ5f5UhboHOJcUwhqbivABX1yFNTbBZJAUDRNORmY2jhWZ94zVXagQ/FnRulU=</Modulus><Exponent>AQAB</Exponent><P>3qPtMt//q0h6SNUrrC+5DSqxCaCa7d8fQNXJwOdSp3dNX5o8j/MNFRIaJzCvFg6BUAwcpltjNERoKgfR/WoWew==</P><Q>0xf6qI1vTUue8HP4CGZGyItjdslkCIS6eS6MWvjsTdbwELLg13uEYBqM8ppdv0SAdsyZAu8XEx1SDehMwRqBbw==</Q><DP>CX3fnO2jzr+WRwifhgW60+7gAVMRh9adVHxIz6qNAYq6h7rhnhl0k1NkPgt7S2tu4+TAS+9VeWL5NeGDeFRPhQ==</DP><DQ>nw8EepkH8vA2NOzNSlb2owoUyl75l0mb0M/4Rlwmgoign5SJwxR5LIkVB4C1fve47MtByGorsuV2/K+7lg3I1Q==</DQ><InverseQ>qxfKZ8UAQDTevKy3D1b9LQdlqKPRGQPYteecFy3atM14wfWBAQICeSAZJzAdHjor4+r+UPN03ZqvbaLjKdJQkQ==</InverseQ><D>hWEBEyTKPtslBLzQxHwEoAfCSogpf2hSZU/SEqqbslCwn7qJudmkUYbHnZcVnRgS3/QfNZPYVPd5bpphi83ooXFm6z0PCwLXpGIz/Ogl9Ui+E836fpN4OZ5wehdFGZr6RLnpPppP7n1wEQ5lDzX43exjBB/8yGizESUf26E/Myk=</D></RSAKeyValue>";
+		public string ConnectionString
+		{
+			get { return Coder.BytesToString(Crypto.Decrypt(Crypto.Type.RSAAES, Coder.StringToBytes(connectionString, Coder.CodePage.Base64), encryptionKey), Coder.CodePage.UTF8); }
+			set { connectionString = Coder.BytesToString(Crypto.Encrypt(Crypto.Type.RSAAES, Coder.StringToBytes(value, Coder.CodePage.UTF8), encryptionKey), Coder.CodePage.Base64); }
+		}
 
 		public DBConnectInfo Copy()
 		{
 			return new DBConnectInfo
 			{
 				Name = Name,
-				ConnectionString = ConnectionString,
+				connectionString = connectionString,
 			};
 		}
 
 		public DbConnection GetConnection()
 		{
+			DbConnection conn;
 			switch (Type)
 			{
-				case DBType.MySQL:
-					{
-						var conn = new MySqlConnection(ConnectionString);
-						conn.Open();
-						return conn;
-					}
+				case DBType.MySQL: conn = new MySqlConnection(ConnectionString); break;
+				case DBType.MSSQL: conn = new SqlConnection(ConnectionString); break;
 				default: throw new ArgumentException("Invalid database type");
 			}
+			conn.Open();
+			return conn;
 		}
 
 		public string Test()
--- TextEdit/Dialogs/DatabaseConnectDialog.xaml.cs
+++ TextEdit/Dialogs/DatabaseConnectDialog.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Xml.Linq;
 using NeoEdit.Common.Transform;
@@ -26,17 +25,12 @@ namespace NeoEdit.TextEdit.Dialogs
 
 		static DatabaseConnectDialog() { UIHelper<DatabaseConnectDialog>.Register(); }
 
-		readonly string dbConfigFile = Path.Combine(Path.GetDirectoryName(typeof(TextEditTabs).Assembly.Location), "DBConfig.dat");
-		const string encryptionKey = "<RSAKeyValue><Modulus>t5X3OepqSDm+YjqiW574OcoYoxLkt4tcEQB7B3+kR9rEVUSUocnQ2pA/rpz91u6crGORz5sUz8rp9NvWhrDeuN/KqZXo8cWGr2l7t6taiAxw+PwQ5f5UhboHOJcUwhqbivABX1yFNTbBZJAUDRNORmY2jhWZ94zVXagQ/FnRulU=</Modulus><Exponent>AQAB</Exponent><P>3qPtMt//q0h6SNUrrC+5DSqxCaCa7d8fQNXJwOdSp3dNX5o8j/MNFRIaJzCvFg6BUAwcpltjNERoKgfR/WoWew==</P><Q>0xf6qI1vTUue8HP4CGZGyItjdslkCIS6eS6MWvjsTdbwELLg13uEYBqM8ppdv0SAdsyZAu8XEx1SDehMwRqBbw==</Q><DP>CX3fnO2jzr+WRwifhgW60+7gAVMRh9adVHxIz6qNAYq6h7rhnhl0k1NkPgt7S2tu4+TAS+9VeWL5NeGDeFRPhQ==</DP><DQ>nw8EepkH8vA2NOzNSlb2owoUyl75l0mb0M/4Rlwmgoign5SJwxR5LIkVB4C1fve47MtByGorsuV2/K+7lg3I1Q==</DQ><InverseQ>qxfKZ8UAQDTevKy3D1b9LQdlqKPRGQPYteecFy3atM14wfWBAQICeSAZJzAdHjor4+r+UPN03ZqvbaLjKdJQkQ==</InverseQ><D>hWEBEyTKPtslBLzQxHwEoAfCSogpf2hSZU/SEqqbslCwn7qJudmkUYbHnZcVnRgS3/QfNZPYVPd5bpphi83ooXFm6z0PCwLXpGIz/Ogl9Ui+E836fpN4OZ5wehdFGZr6RLnpPppP7n1wEQ5lDzX43exjBB/8yGizESUf26E/Myk=</D></RSAKeyValue>";
+		readonly string dbConfigFile = Path.Combine(Path.GetDirectoryName(typeof(TextEditTabs).Assembly.Location), "DBConfig.xml");
 
 		DatabaseConnectDialog()
 		{
 			InitializeComponent();
-			try
-			{
-				var xml = Encoding.UTF8.GetString(Crypto.Decrypt(Crypto.Type.RSAAES, File.ReadAllBytes(dbConfigFile), encryptionKey));
-				DBConnectInfos = new ObservableCollection<DBConnectInfo>(XMLConverter.FromXML<List<DBConnectInfo>>(XElement.Parse(xml)));
-			}
+			try { DBConnectInfos = new ObservableCollection<DBConnectInfo>(XMLConverter.FromXML<List<DBConnectInfo>>(XElement.Load(dbConfigFile))); }
 			catch { DBConnectInfos = new ObservableCollection<DBConnectInfo>(); }
 		}
 
@@ -107,8 +101,10 @@ namespace NeoEdit.TextEdit.Dialogs
 		Result result;
 		void OkClick(object sender, RoutedEventArgs e)
 		{
-			var data = Encoding.UTF8.GetBytes(XMLConverter.ToXML(DBConnectInfos.ToList()).ToString());
-			File.WriteAllBytes(dbConfigFile, Crypto.Encrypt(Crypto.Type.RSAAES, data, encryptionKey));
+			if (DBConnectInfo == null)
+				return;
+
+			XMLConverter.ToXML(DBConnectInfos.ToList()).Save(dbConfigFile);
 
 			result = new Result { DBConnectInfo = DBConnectInfo };
 			DialogResult = true;
