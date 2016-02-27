d46b2863db65d67544bf07071c75868e7c052d9f
rspackman <null@null>
2016-02-27 12:01:48 -0700

    Add SevenZipCompress

---
+++ SevenZipCompress/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using NeoEdit.SevenZip;
+
+namespace SevenZipCompress
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length != 2)
+			{
+				Console.WriteLine($"Usage: {Path.GetFileName(typeof(Program).Assembly.Location)} <Input> <Output>");
+				return;
+			}
+
+			using (var archive = SevenZipArchive.OpenWrite(args[1]))
+				archive.Add(args[0], new List<string> { args[0] });
+		}
+	}
+}
