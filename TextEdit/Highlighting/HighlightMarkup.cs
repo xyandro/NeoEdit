55397e8773b4a17b7c5633e4ebb77e89a68bd3c0
rspackman <randon_s@yahoo.com>
2016-06-07 11:47:35 -0600

    Improve markup highlight (comment/single quote strs)

--- TextEdit/Highlighting/HighlightMarkup.cs
+++ TextEdit/Highlighting/HighlightMarkup.cs
@@ -8,14 +8,16 @@ namespace NeoEdit.TextEdit.Highlighting
 	{
 		const string name = @"[:a-zA-Z\u2070-\u218f\u2c00-\u2fef\u3001-\ud7ff\uf900-\ufdcf\ufdf0-\ufffd][-:a-zA-Z\u2070-\u218f\u2c00-\u2fef\u3001-\ud7ff\uf900-\ufdcf\ufdf0-\ufffd_.0-9\u00b7\u0300-\u036f\u203f-\u2040]*";
 
-		static Regex symbolsRE = new Regex(@"<\??|(\?|/)?>|=");
+		static Regex symbolsRE = new Regex(@"<\??|(\?|/)?>|=", RegexOptions.Compiled);
 		static Brush symbolsBrush = new SolidColorBrush(Color.FromRgb(0, 0, 255));
-		static Regex attributeRE = new Regex($"(?<=<[^>]*){name}");
+		static Regex attributeRE = new Regex($"(?<=<[^>]*){name}", RegexOptions.Compiled);
 		static Brush attributeBrush = new SolidColorBrush(Color.FromRgb(255, 0, 0));
-		static Regex tagRE = new Regex($"(?<=<\\??/?){name}");
+		static Regex tagRE = new Regex($"(?<=<\\??/?){name}", RegexOptions.Compiled);
 		static Brush tagBrush = new SolidColorBrush(Color.FromRgb(163, 21, 21));
-		static Regex stringRE = new Regex(@"""[^<""]*""|'~[<']*'");
+		static Regex stringRE = new Regex(@"""[^<""]*""|'[^<']*'", RegexOptions.Compiled);
 		static Brush stringBrush = new SolidColorBrush(Color.FromRgb(0, 0, 255));
+		static Regex commentRE = new Regex(@"<!--.*?-->", RegexOptions.Compiled);
+		static Brush commentBrush = new SolidColorBrush(Color.FromRgb(0, 128, 0));
 
 		static HighlightMarkup()
 		{
@@ -23,6 +25,7 @@ namespace NeoEdit.TextEdit.Highlighting
 			attributeBrush.Freeze();
 			tagBrush.Freeze();
 			stringBrush.Freeze();
+			commentBrush.Freeze();
 		}
 
 		public override Dictionary<Regex, Brush> GetDictionary()
@@ -33,6 +36,7 @@ namespace NeoEdit.TextEdit.Highlighting
 				[attributeRE] = attributeBrush,
 				[tagRE] = tagBrush,
 				[stringRE] = stringBrush,
+				[commentRE] = commentBrush,
 			};
 		}
 	}
