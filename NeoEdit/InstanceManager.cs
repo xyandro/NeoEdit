9bb44f255e2258f8e8e1caa5d974e46d46788276
rspackman <randon_s@yahoo.com>
2016-08-19 14:30:35 -0600

    Clear modifier keys on startup

--- NeoEdit/InstanceManager.cs
+++ NeoEdit/InstanceManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO.Pipes;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading;
 using NeoEdit.Common.Transform;
 
@@ -15,6 +16,8 @@ namespace NeoEdit
 		[STAThread]
 		static void Main()
 		{
+			ClearModifierKeys();
+
 			var args = Environment.GetCommandLineArgs();
 			var multi = args.Any(arg => arg == "-multi");
 
@@ -56,6 +59,65 @@ namespace NeoEdit
 
 				SetupPipeWait(app);
 			}, null);
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		struct INPUT
+		{
+			public InputType type;
+			[StructLayout(LayoutKind.Sequential)]
+			public struct KEYBDINPUT
+			{
+				public VirtualKeyShort wVk;
+				public short wScan;
+				public KEYEVENTF dwFlags;
+				public int time;
+				public UIntPtr dwExtraInfo;
+			}
+			public KEYBDINPUT ki;
+			[MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+			public byte[] padding;
+
+			public static int Size
+			{
+				get { return Marshal.SizeOf(typeof(INPUT)); }
+			}
+		}
+
+		enum InputType : uint
+		{
+			KEYBOARD = 1,
+		}
+
+		[Flags]
+		enum KEYEVENTF : uint
+		{
+			EXTENDEDKEY = 0x0001,
+			KEYUP = 0x0002,
+		}
+
+		enum VirtualKeyShort : short
+		{
+			LSHIFT = 0xA0,
+			RSHIFT = 0xA1,
+			LCONTROL = 0xA2,
+			RCONTROL = 0xA3,
+		}
+
+		[DllImport("user32.dll")]
+		static extern uint SendInput(int nInputs, [MarshalAs(UnmanagedType.LPArray), In] INPUT[] pInputs, int cbSize);
+
+		static void ClearModifierKeys()
+		{
+			var inputs = new INPUT[4]
+			{
+				new INPUT { type = InputType.KEYBOARD, ki = new INPUT.KEYBDINPUT { wVk = VirtualKeyShort.LSHIFT, dwFlags = KEYEVENTF.KEYUP } },
+				new INPUT { type = InputType.KEYBOARD, ki = new INPUT.KEYBDINPUT { wVk = VirtualKeyShort.RSHIFT, dwFlags = KEYEVENTF.KEYUP } },
+				new INPUT { type = InputType.KEYBOARD, ki = new INPUT.KEYBDINPUT { wVk = VirtualKeyShort.LCONTROL, dwFlags = KEYEVENTF.KEYUP } },
+				new INPUT { type = InputType.KEYBOARD, ki = new INPUT.KEYBDINPUT { wVk = VirtualKeyShort.RCONTROL, dwFlags = KEYEVENTF.KEYUP | KEYEVENTF.EXTENDEDKEY } },
+			};
+
+			SendInput(inputs.Length, inputs, INPUT.Size);
 		}
 	}
 }
