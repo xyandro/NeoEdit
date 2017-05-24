b7910f18bdb83743031605bfdc8fcfde097c6d5f
rspackman <randon_s@yahoo.com>
2017-05-24 16:30:15 -0600

    Improve JSON highlighting

--- TextEdit/Highlighting/HighlightJSON.cs
+++ TextEdit/Highlighting/HighlightJSON.cs
@@ -6,7 +6,7 @@ namespace NeoEdit.TextEdit.Highlighting
 {
 	class HighlightJSON : Highlight
 	{
-		static Regex numberRE = new Regex("[-+0-9.eE]+", RegexOptions.Compiled);
+		static Regex numberRE = new Regex(@"[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?", RegexOptions.Compiled);
 		static Brush numberBrush = new SolidColorBrush(Color.FromRgb(0, 0, 255));
 		static Regex stringRE = new Regex(@"""([^\\""]|\\.)*""", RegexOptions.Compiled);
 		static Brush stringBrush = new SolidColorBrush(Color.FromRgb(163, 21, 21));
