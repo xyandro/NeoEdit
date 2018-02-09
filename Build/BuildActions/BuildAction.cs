0872e32b55a026517f2c42bec83a6add920db380
rspackman <randon_s@yahoo.com>
2018-02-09 11:53:28 -0700

    Update build

--- Build/BuildActions/BuildAction.cs
+++ Build/BuildActions/BuildAction.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Build.BuildActions
 {
@@ -8,7 +11,11 @@ namespace Build.BuildActions
 
 		public override void Run(WriteTextDelegate writeText, string configuration, List<string> platforms)
 		{
-			const string devenv = @"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\devenv.com";
+			var versions = new List<string> { "Enterprise", "Professional" };
+			var template = @"C:\Program Files (x86)\Microsoft Visual Studio\2017\{0}\Common7\IDE\devenv.com";
+			var devenv = versions.Select(version => string.Format(template, version)).FirstOrDefault(path => File.Exists(path));
+			if (devenv == null)
+				throw new Exception("Unable to find devenv");
 			foreach (var platform in platforms)
 			{
 				writeText($"Building {configuration}.{platform}...");
