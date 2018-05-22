20f9b30efc28adf3509643243a214c753ca85a9e
rspackman <randon_s@yahoo.com>
2018-05-22 14:36:37 -0600

    In crop don't assume user wants to keep aspect ratio, plus add hotkeys

--- TextEdit/Dialogs/ImageCropDialog.xaml.cs
+++ TextEdit/Dialogs/ImageCropDialog.xaml.cs
@@ -2,6 +2,7 @@
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
+using System.Windows.Input;
 using System.Windows.Markup;
 using NeoEdit.Common.Expressions;
 using NeoEdit.GUI.Controls;
@@ -48,8 +49,7 @@ namespace NeoEdit.TextEdit.Dialogs
 			OnResetClick();
 		}
 
-		string GetARWidth(string height) => height == "height" ? "width" : $"({height}) * width / height";
-		string GetARHeight(string width) => width == "width" ? "height" : $"({width}) * height / width";
+		bool controlDown => (Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control;
 
 		string GetRatioWidth(string aspectRatio) => $"min(width, height * ({aspectRatio}))";
 		string GetRatioHeight(string aspectRatio) => $"min(width / ({aspectRatio}), height)";
@@ -66,8 +66,6 @@ namespace NeoEdit.TextEdit.Dialogs
 			if (oldWidthExpression == null)
 				return;
 
-			if (HeightExpression == GetARHeight(oldWidthExpression))
-				HeightExpression = GetARHeight(WidthExpression);
 			if (XExpression == GetXLeft(oldWidthExpression))
 				XExpression = GetXLeft(WidthExpression);
 			else if (XExpression == GetXCenter(oldWidthExpression))
@@ -81,8 +79,6 @@ namespace NeoEdit.TextEdit.Dialogs
 			if (oldHeightExpression == null)
 				return;
 
-			if (WidthExpression == GetARWidth(oldHeightExpression))
-				WidthExpression = GetARWidth(HeightExpression);
 			if (YExpression == GetYTop(oldHeightExpression))
 				YExpression = GetYTop(HeightExpression);
 			else if (YExpression == GetYCenter(oldHeightExpression))
@@ -105,18 +101,45 @@ namespace NeoEdit.TextEdit.Dialogs
 		void OnChangeAnchor(object sender, RoutedEventArgs e)
 		{
 			var anchor = ((sender as FrameworkElement).Tag as string).Split(',');
-			switch (anchor[0])
+			SetAnchor(anchor[0], anchor[1]);
+		}
+
+		void SetAnchor(string vertAnchor, string horizAnchor)
+		{
+			switch (vertAnchor)
 			{
 				case "Top": YExpression = GetYTop(HeightExpression); break;
 				case "Center": YExpression = GetYCenter(HeightExpression); break;
 				case "Bottom": YExpression = GetYBottom(HeightExpression); break;
 			}
-			switch (anchor[1])
+			switch (horizAnchor)
 			{
 				case "Left": XExpression = GetXLeft(WidthExpression); break;
 				case "Center": XExpression = GetXCenter(WidthExpression); break;
 				case "Right": XExpression = GetXRight(WidthExpression); break;
 			}
+		}
+
+		protected override void OnPreviewKeyDown(KeyEventArgs e)
+		{
+			if (controlDown)
+			{
+				e.Handled = true;
+				switch (e.Key)
+				{
+					case Key.NumPad7: case Key.Home: SetAnchor("Top", "Left"); break;
+					case Key.NumPad8: case Key.Up: SetAnchor("Top", "Center"); break;
+					case Key.NumPad9: case Key.PageUp: SetAnchor("Top", "Right"); break;
+					case Key.NumPad4: case Key.Left: SetAnchor("Center", "Left"); break;
+					case Key.NumPad5: case Key.Clear: SetAnchor("Center", "Center"); break;
+					case Key.NumPad6: case Key.Right: SetAnchor("Center", "Right"); break;
+					case Key.NumPad1: case Key.End: SetAnchor("Bottom", "Left"); break;
+					case Key.NumPad2: case Key.Down: SetAnchor("Bottom", "Center"); break;
+					case Key.NumPad3: case Key.PageDown: SetAnchor("Bottom", "Right"); break;
+					default: e.Handled = false; break;
+				}
+			}
+			base.OnPreviewKeyDown(e);
 		}
 
 		internal bool IsChecked(string position)
@@ -149,7 +172,7 @@ namespace NeoEdit.TextEdit.Dialogs
 			HeightExpression = "height";
 			XExpression = GetXCenter(WidthExpression);
 			YExpression = GetYCenter(HeightExpression);
-			AspectRatio = "5/7";
+			AspectRatio = "width/height";
 		}
 
 		void ExpressionHelp(object sender, RoutedEventArgs e) => ExpressionHelpDialog.Display(Variables);
