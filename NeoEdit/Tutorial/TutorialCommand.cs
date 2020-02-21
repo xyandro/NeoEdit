e90c81ce284e9bec3ab8648f44aa82436c9adde0
rspackman <randon_s@yahoo.com>
2020-02-21 10:58:35 -0700

    Prefer first accelerator (but can use others)

--- NeoEdit/Tutorial/TutorialCommand.cs
+++ NeoEdit/Tutorial/TutorialCommand.cs
@@ -12,43 +12,60 @@ namespace NeoEdit.Program.Tutorial
 
 		readonly static NEMenu neMenu = new NEMenu();
 
+		NECommand commandEnum;
 		public NECommand CommandEnum
 		{
 			set
 			{
-				var item = UIHelper.FindLogicalChildren<NEMenuItem>(neMenu).FirstOrDefault(mi => mi.CommandEnum == value);
-				if (item == null)
-					throw new Exception($"Command not found in menu: {value}");
-
-				var menuItem = item as MenuItem;
-				var click = "";
-				var key = "";
-				while (menuItem != null)
-				{
-					var header = menuItem.Header.ToString();
-
-					if (click.Length != 0)
-						click = $"{NextClick}{click}";
-					click = header.Replace("_", "") + click;
-
-					var accelIndex = header.IndexOf('_');
-					if (accelIndex != -1)
-					{
-						if (key.Length != 0)
-							key = $"{NextKey}{key}";
-						key = $"{char.ToUpperInvariant(header[accelIndex + 1])}{key}";
-					}
-
-					menuItem = menuItem.Parent as MenuItem;
-				}
-				click = $"Menu{NextClick}{click}";
-				key = $"Alt+{key}";
-
-				if (!string.IsNullOrEmpty(item.InputGestureText))
-					key = item.InputGestureText;
-
-				Text = $"Click {click} (or press {key})";
+				commandEnum = value;
+				SetText();
 			}
+		}
+
+		string accel;
+		public string Accel
+		{
+			set
+			{
+				accel = value;
+				SetText();
+			}
+		}
+
+		void SetText()
+		{
+			var item = UIHelper.FindLogicalChildren<NEMenuItem>(neMenu).FirstOrDefault(mi => mi.CommandEnum == commandEnum);
+			if (item == null)
+				throw new Exception($"Command not found in menu: {commandEnum}");
+
+			var menuItem = item as MenuItem;
+			var click = "";
+			var key = "";
+			while (menuItem != null)
+			{
+				var header = menuItem.Header.ToString();
+
+				if (click.Length != 0)
+					click = $"{NextClick}{click}";
+				click = header.Replace("_", "") + click;
+
+				var accelIndex = header.IndexOf('_');
+				if (accelIndex != -1)
+				{
+					if (key.Length != 0)
+						key = $"{NextKey}{key}";
+					key = $"{char.ToUpperInvariant(header[accelIndex + 1])}{key}";
+				}
+
+				menuItem = menuItem.Parent as MenuItem;
+			}
+			click = $"Menu{NextClick}{click}";
+			key = $"Alt+{key}";
+
+			if (!string.IsNullOrEmpty(item.InputGestureText))
+				key = item.InputGestureText.Split(new string[] { ", " }, StringSplitOptions.None).OrderBy(s => !s.Contains(accel ?? "")).First();
+
+			Text = $"{click} (or press {key})";
 		}
 	}
 }
