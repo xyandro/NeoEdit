f1bdccf799f79b5bfc97dff8acf6244cbdcdd14d
rspackensen <randon_s@yahoo.com>
2020-02-28 14:22:52 -0700

    Handle UTF8 chars in ChangeLog.txt

--- NeoEdit/ChangeLog.ps1
+++ NeoEdit/ChangeLog.ps1
@@ -1,6 +1,6 @@
 $data = (git log --pretty=oneline)
 For ($i=0; $i -lt $data.Count; $i++)
 {
-	$data[$i] = ($data.Count - $i).ToString() + "	" + $data[$i].SubString(41)
+	$data[$i] = ($data.Count - $i).ToString() + "	" + [Text.Encoding]::UTF8.GetString([Text.Encoding]::GetEncoding(437).GetBytes($data[$i].SubString(41)))
 }
 [System.IO.File]::WriteAllLines($args[0], $data, [Text.Encoding]::UTF8)
