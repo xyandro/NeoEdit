7188b48333e887b79ba78688bab3a8bd0a077bb1
Randon Spackensen <randon.spackensen@gmail.com>
2020-03-05 03:28:40 -0700

    Update build to use 2019

--- Build/BuildActions/BuildAction.cs
+++ Build/BuildActions/BuildAction.cs
@@ -11,8 +11,8 @@ namespace Build.BuildActions
 
 		public override void Run(WriteTextDelegate writeText, string configuration, List<string> platforms)
 		{
-			var versions = new List<string> { "Enterprise", "Professional" };
-			var template = @"C:\Program Files (x86)\Microsoft Visual Studio\2017\{0}\Common7\IDE\devenv.com";
+			var versions = new List<string> { "Community", "Enterprise", "Professional" };
+			var template = @"C:\Program Files (x86)\Microsoft Visual Studio\2019\{0}\Common7\IDE\devenv.com";
 			var devenv = versions.Select(version => string.Format(template, version)).FirstOrDefault(path => File.Exists(path));
 			if (devenv == null)
 				throw new Exception("Unable to find devenv");
