9d3f14d04205e0bcc34d7d1d3fb11bfe269201c7
rspackensen <randon_s@yahoo.com>
2020-03-11 10:09:14 -0600

    Add parallel code path (to eventually replace existing one)

---
+++ NeoEdit/TextEditorData-Select.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace NeoEdit.Program
+{
+	partial class TextEditorData
+	{
+		void Command_Select_All() => SetSelections(new List<Range> { new Range(Text.Length, 0) });
+
+		void Command_Select_Lines()
+		{
+			var lineSets = Selections.AsParallel().Select(range => new { start = TextView.GetPositionLine(range.Start), end = TextView.GetPositionLine(Math.Max(range.Start, range.End - 1)) }).ToList();
+
+			var hasLine = new bool[TextView.NumLines];
+			foreach (var set in lineSets)
+				for (var ctr = set.start; ctr <= set.end; ++ctr)
+					hasLine[ctr] = true;
+
+			var lines = new List<int>();
+			for (var line = 0; line < hasLine.Length; ++line)
+				if (hasLine[line])
+					lines.Add(line);
+
+			SetSelections(lines.AsParallel().AsOrdered().Select(line => Range.FromIndex(TextView.GetPosition(line, 0), TextView.GetLineLength(line))).ToList());
+		}
+	}
+}
---
+++ NeoEdit/TextEditorData.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NeoEdit.Program
+{
+	partial class TextEditorData
+	{
+		NEText text, newText;
+		public NEText Text
+		{
+			get => newText ?? text;
+			private set
+			{
+				newText = value;
+				newTextView = new NETextView(newText);
+			}
+		}
+
+		void ReplaceText(List<Range> ranges, List<string> strs)
+		{
+			Text = Text.Replace(ranges, strs);
+
+			var translateMap = GetTranslateMap(ranges, strs, new List<List<Range>> { Selections }.Concat(Enumerable.Range(1, 9).Select(region => GetRegions(region))).ToList());
+			SetSelections(Translate(Selections, translateMap));
+			for (var region = 1; region <= 9; ++region)
+				SetRegions(region, Translate(GetRegions(region), translateMap));
+		}
+
+		NETextView textView, newTextView;
+		public NETextView TextView => newTextView ?? textView;
+
+		List<Range> selections, newSelections;
+		public List<Range> Selections
+		{
+			get => newSelections ?? selections;
+			set => newSelections = value;
+		}
+
+		public void SetSelections(List<Range> selections) => Selections = DeOverlap(selections);
+
+		readonly List<Range>[] regions = Enumerable.Range(0, 9).Select(num => new List<Range>()).ToArray();
+		readonly List<Range>[] newRegions = new List<Range>[9];
+		public List<Range> GetRegions(int region)
+		{
+			if ((region < 1) || (region > 9))
+				throw new IndexOutOfRangeException($"Invalid region: {region}");
+			return newRegions[region - 1] ?? regions[region - 1];
+		}
+
+		public void SetRegions(int region, List<Range> regions)
+		{
+			if ((region < 1) || (region > 9))
+				throw new IndexOutOfRangeException($"Invalid region: {region}");
+			newRegions[region - 1] = DeOverlap(regions);
+		}
+
+		public void Commit()
+		{
+			text = newText ?? text;
+			textView = newTextView ?? textView;
+			selections = newSelections ?? selections;
+			for (var ctr = 0; ctr < regions.Length; ++ctr)
+				regions[ctr] = newRegions[ctr] ?? regions[ctr];
+		}
+
+		public void Rollback()
+		{
+			newText = null;
+			newTextView = null;
+			newSelections = null;
+			for (var ctr = 0; ctr < regions.Length; ++ctr)
+				newRegions[ctr] = null;
+		}
+
+		#region Translate
+		static int[] GetTranslateNums(List<List<Range>> rangeLists)
+		{
+			var nums = new int[rangeLists.Sum(rangeList => rangeList.Count * 2)];
+			var numsStart = 0;
+			foreach (var rangeList in rangeLists)
+			{
+				var size = Math.Max(65536, (rangeList.Count + 31) / 32);
+				Helpers.PartitionedParallelForEach(rangeList.Count, size, (start, end) =>
+				{
+					var numPos = numsStart + start * 2;
+					for (var r = start; r < end; ++r)
+					{
+						nums[numPos++] = rangeList[r].Start;
+						nums[numPos++] = rangeList[r].End;
+					}
+				});
+				numsStart += rangeList.Count * 2;
+			}
+
+			Array.Sort(nums);
+
+			var outPos = -1;
+			for (var inPos = 0; inPos < nums.Length; ++inPos)
+			{
+				if ((outPos != -1) && (nums[inPos] == nums[outPos]))
+					continue;
+				nums[++outPos] = nums[inPos];
+			}
+
+			Array.Resize(ref nums, outPos + 1);
+			return nums;
+		}
+
+		static Tuple<int[], int[]> GetTranslateMap(List<Range> replaceRanges, List<string> strs, List<List<Range>> rangeLists)
+		{
+			var translateNums = GetTranslateNums(rangeLists);
+			var translateResults = new int[translateNums.Length];
+			var replaceRange = 0;
+			var offset = 0;
+			var current = 0;
+			while (current < translateNums.Length)
+			{
+				int start = int.MaxValue, end = int.MaxValue, length = 0;
+				if (replaceRange < replaceRanges.Count)
+				{
+					start = replaceRanges[replaceRange].Start;
+					end = replaceRanges[replaceRange].End;
+					length = strs[replaceRange].Length;
+				}
+
+				if (translateNums[current] >= end)
+				{
+					offset += start - end + length;
+					++replaceRange;
+					continue;
+				}
+
+				var value = translateNums[current];
+				if ((value > start) && (value < end))
+					value = start + length;
+
+				translateResults[current] = value + offset;
+				++current;
+			}
+
+			return Tuple.Create(translateNums, translateResults);
+		}
+
+		static List<Range> Translate(List<Range> ranges, Tuple<int[], int[]> translateMap)
+		{
+			var result = Helpers.PartitionedParallelForEach<Range>(ranges.Count, Math.Max(65536, (ranges.Count + 31) / 32), (start, end, list) =>
+			{
+				var current = 0;
+				for (var ctr = start; ctr < end; ++ctr)
+				{
+					current = Array.IndexOf(translateMap.Item1, ranges[ctr].Start, current);
+					var startPos = current;
+					current = Array.IndexOf(translateMap.Item1, ranges[ctr].End, current);
+					if (ranges[ctr].Cursor < ranges[ctr].Anchor)
+						list.Add(new Range(translateMap.Item2[startPos], translateMap.Item2[current]));
+					else
+						list.Add(new Range(translateMap.Item2[current], translateMap.Item2[startPos]));
+				}
+			});
+			return result;
+		}
+		#endregion
+
+		#region DeOverlap
+		enum DeOverlapStep
+		{
+			Sort,
+			DeOverlap,
+			Done,
+		}
+
+		static List<Range> DeOverlap(List<Range> items)
+		{
+			while (true)
+			{
+				switch (GetDeOverlapStep(items))
+				{
+					case DeOverlapStep.Sort: items = items.OrderBy(range => range.Start).ThenBy(range => range.End).ToList(); break;
+					case DeOverlapStep.DeOverlap: return DoDeOverlap(items);
+					case DeOverlapStep.Done: return items;
+					default: throw new Exception("Invalid step");
+				}
+			}
+		}
+
+		static DeOverlapStep GetDeOverlapStep(List<Range> items)
+		{
+			var result = DeOverlapStep.Done;
+			for (var ctr = 1; ctr < items.Count; ++ctr)
+			{
+				if ((items[ctr].Start < items[ctr - 1].Start) || ((items[ctr].Start == items[ctr - 1].Start) && (items[ctr].End < items[ctr - 1].End)))
+					return DeOverlapStep.Sort;
+
+				if ((items[ctr].Start < items[ctr - 1].End) || ((items[ctr].Start == items[ctr - 1].Start) && (items[ctr].End == items[ctr - 1].End)))
+					result = DeOverlapStep.DeOverlap;
+			}
+
+			return result;
+		}
+
+		static List<Range> DoDeOverlap(List<Range> items)
+		{
+			var result = new List<Range>();
+
+			using (var enumerator = items.GetEnumerator())
+			{
+				var last = default(Range);
+
+				while (true)
+				{
+					var range = enumerator.MoveNext() ? enumerator.Current : null;
+
+					if ((last != null) && ((range == null) || (last.Start != range.Start)))
+					{
+						if ((range == null) || (last.End <= range.Start))
+							result.Add(last);
+						else if (last.Cursor < last.Anchor)
+							result.Add(new Range(last.Start, range.Start));
+						else
+							result.Add(new Range(range.Start, last.Start));
+						last = null;
+					}
+
+					if (range == null)
+						break;
+
+					if ((last != null) && (range.End <= last.End))
+						continue;
+
+					last = range;
+				}
+			}
+
+			return result;
+		}
+		#endregion
+	}
+}
