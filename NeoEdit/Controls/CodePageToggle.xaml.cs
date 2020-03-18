63b944ca2a8927bcf2bd150cab5a874ad24c47a2
Randon Spackensen <randon.spackensen@gmail.com>
2020-03-17 21:07:31 -0600

    Refactor code pages dialog

---
+++ NeoEdit/Controls/CodePageToggle.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using NeoEdit.Program.Controls;
+
+namespace NeoEdit.Program.Controls
+{
+	partial class CodePageToggle
+	{
+		[DepProp]
+		public string OnText { get { return UIHelper<CodePageToggle>.GetPropValue<string>(this); } set { UIHelper<CodePageToggle>.SetPropValue(this, value); } }
+		[DepProp]
+		public string OffText { get { return UIHelper<CodePageToggle>.GetPropValue<string>(this); } set { UIHelper<CodePageToggle>.SetPropValue(this, value); } }
+
+		static CodePageToggle() => UIHelper<CodePageToggle>.Register();
+
+		public bool IsChecked { get => toggleButton.IsChecked == true; set => toggleButton.IsChecked = value; }
+		public string Text { set { OnText = OffText = value; } }
+
+		public event RoutedEventHandler Click;
+
+		public CodePageToggle()
+		{
+			InitializeComponent();
+			OnText = "Enabled";
+			OffText = "Disabled";
+		}
+
+		void OnClick(object sender, RoutedEventArgs e) => Click?.Invoke(this, e);
+	}
+}
--- NeoEdit/Dialogs/CodePagesDialog.xaml.cs
+++ NeoEdit/Dialogs/CodePagesDialog.xaml.cs
@@ -1,150 +1,127 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
-using NeoEdit.Program;
+using System.Windows.Controls.Primitives;
 using NeoEdit.Program.Controls;
-using NeoEdit.Program.Parsing;
 using NeoEdit.Program.Transform;
 
 namespace NeoEdit.Program.Dialogs
 {
-	public class CodePagesDialogCheckBox : CheckBox
-	{
-		Coder.CodePage codePage = Coder.CodePage.None;
-		public Coder.CodePage CodePage
-		{
-			get => codePage;
-			set
-			{
-				codePage = value;
-				Content = Coder.IsStr(codePage) ? Coder.GetDescription(codePage) : codePage.ToString();
-			}
-		}
-	}
-
 	partial class CodePagesDialog
 	{
 		public static IReadOnlyCollection<Coder.CodePage> DefaultCodePages { get; } = new HashSet<Coder.CodePage> { Coder.CodePage.UInt8, Coder.CodePage.UInt16LE, Coder.CodePage.UInt32LE, Coder.CodePage.UInt64LE, Coder.CodePage.Int8, Coder.CodePage.Int16LE, Coder.CodePage.Int32LE, Coder.CodePage.Int64LE, Coder.DefaultCodePage, Coder.CodePage.UTF8, Coder.CodePage.UTF16LE };
 
-		HashSet<Coder.CodePage> result { get; set; }
-
-		[DepProp]
-		public bool ShowLE { get { return UIHelper<CodePagesDialog>.GetPropValue<bool>(this); } set { UIHelper<CodePagesDialog>.SetPropValue(this, value); } }
-		[DepProp]
-		public bool ShowBE { get { return UIHelper<CodePagesDialog>.GetPropValue<bool>(this); } set { UIHelper<CodePagesDialog>.SetPropValue(this, value); } }
-		[DepProp]
-		public bool ShowInt { get { return UIHelper<CodePagesDialog>.GetPropValue<bool>(this); } set { UIHelper<CodePagesDialog>.SetPropValue(this, value); } }
-		[DepProp]
-		public bool ShowFloat { get { return UIHelper<CodePagesDialog>.GetPropValue<bool>(this); } set { UIHelper<CodePagesDialog>.SetPropValue(this, value); } }
-		[DepProp]
-		public bool ShowStr { get { return UIHelper<CodePagesDialog>.GetPropValue<bool>(this); } set { UIHelper<CodePagesDialog>.SetPropValue(this, value); } }
-		[DepProp]
-		ObservableCollection<CodePagesDialogCheckBox> EncodingCheckBoxes { get { return UIHelper<CodePagesDialog>.GetPropValue<ObservableCollection<CodePagesDialogCheckBox>>(this); } set { UIHelper<CodePagesDialog>.SetPropValue(this, value); } }
-
-		static bool savedShowLE = true;
-		static bool savedShowBE = false;
-		static bool savedShowInt = true;
-		static bool savedShowFloat = false;
-		static bool savedShowStr = true;
-		readonly List<CodePagesDialogCheckBox> checkBoxes;
-		readonly static HashSet<Coder.CodePage> savedCodePages = new HashSet<Coder.CodePage>(DefaultCodePages);
-
-		static CodePagesDialog() => UIHelper<CodePagesDialog>.Register();
-
-		CodePagesDialog(HashSet<Coder.CodePage> startCodePages = null)
+		readonly HashSet<string> codePages = new HashSet<string>();
+		readonly List<FrameworkElement> checkBoxes = new List<FrameworkElement>();
+		CodePagesDialog(HashSet<Coder.CodePage> startCodePages)
 		{
 			InitializeComponent();
 
-			EncodingCheckBoxes = new ObservableCollection<CodePagesDialogCheckBox>(Coder.GetStringCodePages().Select(codePage => new CodePagesDialogCheckBox { CodePage = codePage }));
-			checkBoxes = this.FindLogicalChildren<CodePagesDialogCheckBox>().Concat(EncodingCheckBoxes).ToList();
-
-			ShowLE = savedShowLE;
-			ShowBE = savedShowBE;
-			ShowInt = savedShowInt;
-			ShowFloat = savedShowFloat;
-			ShowStr = savedShowStr;
-			startCodePages = startCodePages ?? savedCodePages;
-			foreach (var checkBox in checkBoxes)
-				checkBox.IsChecked = startCodePages.Contains(checkBox.CodePage);
-		}
-
-		void CodePagesPreviewKeyDown(object sender, KeyEventArgs e)
-		{
-			if (e.Key == Key.Space)
+			var row = 0;
+			foreach (var codePage in Coder.GetStringCodePages())
 			{
-				var selected = codePages.SelectedItems.OfType<CodePagesDialogCheckBox>().ToList();
-				var status = !selected.All(item => item.IsChecked == true);
-				selected.ForEach(item => item.IsChecked = status);
-				e.Handled = true;
-			}
-		}
+				stringsGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
 
-		void SelectAllNoneNumerics(object sender, RoutedEventArgs e)
-		{
-			var all = (e.Source as FrameworkElement).Tag as string == "all";
-			var children = (e.Source as FrameworkElement).FindParent<GroupBox>().FindLogicalChildren<CodePagesDialogCheckBox>().ToList();
-			foreach (var child in children)
-				if (child.IsVisible)
-					child.IsChecked = all;
-		}
+				var toggleButton = new CodePageToggle { Text = Coder.GetDescription(codePage), Tag = codePage.ToString(), Padding = new Thickness(1) };
+				toggleButton.Click += OnClick;
+				Grid.SetRow(toggleButton, row);
+				Grid.SetColumn(toggleButton, 0);
+				stringsGrid.Children.Add(toggleButton);
 
-		void SelectAllNoneStrings(object sender, RoutedEventArgs e)
-		{
-			var all = (e.Source as FrameworkElement).Tag as string == "all";
-			foreach (var checkBox in EncodingCheckBoxes)
-				checkBox.IsChecked = all;
-		}
+				checkBoxes.Add(toggleButton);
 
-		void SelectAllNoneGlobal(object sender, RoutedEventArgs e)
-		{
-			var all = (e.Source as FrameworkElement).Tag as string == "all";
-			if (all)
-				ShowLE = ShowBE = ShowInt = ShowFloat = ShowStr = true;
-			foreach (var checkBox in checkBoxes)
-				checkBox.IsChecked = all;
-		}
-
-		void Reset(object sender = null, RoutedEventArgs e = null)
-		{
-			ShowLE = ShowInt = ShowStr = true;
-			ShowBE = ShowFloat = false;
-			foreach (var checkBox in checkBoxes)
-				checkBox.IsChecked = DefaultCodePages.Contains(checkBox.CodePage);
-		}
-
-		void OkClick(object sender, RoutedEventArgs e)
-		{
-			var codePages = new HashSet<Coder.CodePage>();
-			foreach (var checkBox in checkBoxes)
-			{
-				if (checkBox.IsChecked != true)
-					continue;
-				var str = Coder.IsStr(checkBox.CodePage);
-				if ((str) && (!ShowStr))
-					continue;
-				if ((!str) && (!checkBox.IsVisible))
-					continue;
-				codePages.Add(checkBox.CodePage);
+				++row;
 			}
 
-			if (codePages.Count == 0)
+			Strings.Tag = string.Join("|", checkBoxes.Select(x => x.Tag));
+
+			checkBoxes.AddRange(new List<FrameworkElement> { LE, SIntLE, UIntLE, Int8LE, Int16LE, Int32LE, Int64LE, UInt8LE, UInt16LE, UInt32LE, UInt64LE, BE, SIntBE, UIntBE, Int8BE, Int16BE, Int32BE, Int64BE, UInt8BE, UInt16BE, UInt32BE, UInt64BE, Float, Single, Double, Strings });
+
+			SetCodePages(startCodePages);
+		}
+
+		void SetCodePages(IReadOnlyCollection<Coder.CodePage> startCodePages)
+		{
+			codePages.Clear();
+			foreach (var startCodePage in startCodePages)
+			{
+				var codePage = startCodePage.ToString();
+				if (!codePage.EndsWith("Int8"))
+					codePages.Add(codePage);
+				else
+				{
+					var hasLE = startCodePages.Intersect(Coder.GetNumericCodePages()).Any(x => x.ToString().EndsWith("LE"));
+					var hasBE = startCodePages.Intersect(Coder.GetNumericCodePages()).Any(x => x.ToString().EndsWith("BE"));
+					if ((hasLE) || (!hasBE))
+						codePages.Add(codePage + "LE");
+					if (hasBE)
+						codePages.Add(codePage + "BE");
+				}
+			}
+			UpdateCheckBoxes();
+		}
+
+		void OnClick(object sender, RoutedEventArgs e)
+		{
+			if (!(e.Source is ToggleButton button))
 				return;
 
-			result = codePages;
+			var codePages = ((sender as FrameworkElement).Tag as string).Split('|').ToList();
+			if (button.IsChecked == true)
+				codePages.ForEach(codePage => this.codePages.Add(codePage));
+			else
+				codePages.ForEach(codePage => this.codePages.Remove(codePage));
 
-			savedShowLE = ShowLE;
-			savedShowBE = ShowBE;
-			savedShowInt = ShowInt;
-			savedShowFloat = ShowFloat;
-			savedShowStr = ShowStr;
-			savedCodePages.Clear();
+			UpdateCheckBoxes();
+		}
+
+		void UpdateCheckBoxes()
+		{
 			foreach (var checkBox in checkBoxes)
-				if (checkBox.IsChecked == true)
-					savedCodePages.Add(checkBox.CodePage);
+			{
+				var tags = (checkBox.Tag as string).Split('|').ToList();
+				var found = tags.Select(codePage => codePages.Contains(codePage)).Distinct().Take(2).ToList();
+				var state = found.Count == 1 ? found[0] : default(bool?);
+				SetCheckbox(checkBox, state);
+			}
+		}
+
+		void SetCheckbox(FrameworkElement checkBox, bool? state)
+		{
+			if (checkBox is CheckBox cb)
+				cb.IsChecked = state;
+			else if (checkBox is CodePageToggle cpt)
+				cpt.IsChecked = state.Value;
+		}
+
+		void CheckAllNone(object sender, RoutedEventArgs e)
+		{
+			if (!(sender is FrameworkElement element))
+				return;
+			if ((element.Tag as string) == "All")
+				checkBoxes.OfType<FrameworkElement>().SelectMany(x => (x.Tag as string).Split('|')).Distinct().ForEach(codePage => codePages.Add(codePage));
+			else
+				codePages.Clear();
+			UpdateCheckBoxes();
+		}
+
+		void Reset(object sender = null, RoutedEventArgs e = null) => SetCodePages(DefaultCodePages);
+
+		HashSet<Coder.CodePage> result;
+		void OkClick(object sender, RoutedEventArgs e)
+		{
+			result = new HashSet<Coder.CodePage>();
+			foreach (var codePage in codePages)
+			{
+				switch (codePage)
+				{
+					case "UInt8LE": case "UInt8BE": result.Add(Coder.CodePage.UInt8); break;
+					case "Int8LE": case "Int8BE": result.Add(Coder.CodePage.Int8); break;
+					default: result.Add(Helpers.ParseEnum<Coder.CodePage>(codePage)); break;
+				}
+			}
 
 			DialogResult = true;
 		}
@@ -155,6 +132,17 @@ namespace NeoEdit.Program.Dialogs
 			if (!find.ShowDialog())
 				throw new OperationCanceledException();
 			return find.result;
+		}
+	}
+
+	public class NoResizeScrollViewer : ScrollViewer
+	{
+		protected override Size MeasureOverride(Size constraint) => new Size(base.MeasureOverride(constraint).Width, 0);
+
+		protected override Size ArrangeOverride(Size arrangeBounds)
+		{
+			Height = arrangeBounds.Height;
+			return base.ArrangeOverride(arrangeBounds);
 		}
 	}
 }
