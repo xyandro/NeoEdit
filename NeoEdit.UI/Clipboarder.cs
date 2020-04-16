53d1d4ea510200833ef732f5be2086dc1b4b39ad
rspackensen <randon_s@yahoo.com>
2020-04-16 16:32:49 -0600

    Entire clipboard routine executes on main thread

--- NeoEdit.UI/Clipboarder.cs
+++ NeoEdit.UI/Clipboarder.cs
@@ -62,57 +62,61 @@ namespace NeoEdit.UI
 			if (!clipboardChanged)
 				return;
 
-			var dataObj = dispatcher.Invoke(() =>
+			dispatcher.Invoke(() =>
 			{
-				try { return Clipboard.GetDataObject(); }
-				catch { return null; }
-			});
-			if (dataObj == null)
-				return;
-
-			if (dataObj.GetData(typeof(NEClipboard)) as int? == PID)
-			{
-				clipboardChanged = false;
-				return;
-			}
-
-			var result = new NEClipboard();
-
-			var dropList = (dataObj.GetData(DataFormats.FileDrop) as string[])?.OrderBy(Helpers.SmartComparer(false)).ToList();
-			if ((dropList != null) && (dropList.Count != 0))
-			{
-				var isCut = false;
-				var dropEffectStream = dataObj.GetData("Preferred DropEffect");
-				if (dropEffectStream is MemoryStream)
+				try
 				{
-					try
+					var dataObj = Clipboard.GetDataObject();
+
+					if (dataObj == null)
+						return;
+
+					if (dataObj.GetData(typeof(NEClipboard)) as int? == PID)
 					{
-						var dropEffect = (DragDropEffects)BitConverter.ToInt32(((MemoryStream)dropEffectStream).ToArray(), 0);
-						isCut = dropEffect.HasFlag(DragDropEffects.Move);
+						clipboardChanged = false;
+						return;
 					}
-					catch { }
+
+					var result = new NEClipboard();
+
+					var dropList = (dataObj.GetData(DataFormats.FileDrop) as string[])?.OrderBy(Helpers.SmartComparer(false)).ToList();
+					if ((dropList != null) && (dropList.Count != 0))
+					{
+						var isCut = false;
+						var dropEffectStream = dataObj.GetData("Preferred DropEffect");
+						if (dropEffectStream is MemoryStream)
+						{
+							try
+							{
+								var dropEffect = (DragDropEffects)BitConverter.ToInt32(((MemoryStream)dropEffectStream).ToArray(), 0);
+								isCut = dropEffect.HasFlag(DragDropEffects.Move);
+							}
+							catch { }
+						}
+						result.Add(new List<string>(dropList));
+						result.IsCut = isCut;
+					}
+					else
+					{
+						var str = dataObj.GetData(DataFormats.UnicodeText) as string ?? dataObj.GetData(DataFormats.OemText) as string ?? dataObj.GetData(DataFormats.Text) as string ?? dataObj.GetData(typeof(string)) as string;
+
+						if ((str == null) && (dataObj.GetData(DataFormats.Bitmap, true) is BitmapSource image))
+						{
+							var bmp = new Bitmap(image.PixelWidth, image.PixelHeight, PixelFormat.Format32bppPArgb);
+							var data = bmp.LockBits(new Rectangle(System.Drawing.Point.Empty, bmp.Size), ImageLockMode.WriteOnly, PixelFormat.Format32bppPArgb);
+							image.CopyPixels(Int32Rect.Empty, data.Scan0, data.Height * data.Stride, data.Stride);
+							bmp.UnlockBits(data);
+							str = Coder.BitmapToString(bmp);
+						}
+
+						result.Add(new List<string> { str });
+					}
+
+					NEClipboard.Current = systemClipboard = result;
+					clipboardChanged = false;
 				}
-				result.Add(new List<string>(dropList));
-				result.IsCut = isCut;
-			}
-			else
-			{
-				var str = dataObj.GetData(DataFormats.UnicodeText) as string ?? dataObj.GetData(DataFormats.OemText) as string ?? dataObj.GetData(DataFormats.Text) as string ?? dataObj.GetData(typeof(string)) as string;
-
-				if ((str == null) && (dataObj.GetData(DataFormats.Bitmap, true) is BitmapSource image))
-				{
-					var bmp = new Bitmap(image.PixelWidth, image.PixelHeight, PixelFormat.Format32bppPArgb);
-					var data = bmp.LockBits(new Rectangle(System.Drawing.Point.Empty, bmp.Size), ImageLockMode.WriteOnly, PixelFormat.Format32bppPArgb);
-					image.CopyPixels(Int32Rect.Empty, data.Scan0, data.Height * data.Stride, data.Stride);
-					bmp.UnlockBits(data);
-					str = Coder.BitmapToString(bmp);
-				}
-
-				result.Add(new List<string> { str });
-			}
-
-			NEClipboard.Current = systemClipboard = result;
-			clipboardChanged = false;
+				catch { }
+			});
 		}
 
 		static public void SetSystem(Dispatcher dispatcher)
@@ -120,23 +124,23 @@ namespace NeoEdit.UI
 			if ((clipboardChanged) || (NEClipboard.Current == systemClipboard))
 				return;
 
-			var dataObj = new DataObject();
-
-			dataObj.SetText(NEClipboard.Current.String, TextDataFormat.UnicodeText);
-			dataObj.SetData(typeof(NEClipboard), PID);
-
-			if (NEClipboard.Current.IsCut.HasValue)
-			{
-				var dropList = new StringCollection();
-				dropList.AddRange(NEClipboard.Current.Strings.ToArray());
-				dataObj.SetFileDropList(dropList);
-				dataObj.SetData("Preferred DropEffect", new MemoryStream(BitConverter.GetBytes((int)(NEClipboard.Current.IsCut == true ? DragDropEffects.Move : DragDropEffects.Copy | DragDropEffects.Link))));
-			}
-
 			dispatcher.Invoke(() =>
 			{
 				try
 				{
+					var dataObj = new DataObject();
+
+					dataObj.SetText(NEClipboard.Current.String, TextDataFormat.UnicodeText);
+					dataObj.SetData(typeof(NEClipboard), PID);
+
+					if (NEClipboard.Current.IsCut.HasValue)
+					{
+						var dropList = new StringCollection();
+						dropList.AddRange(NEClipboard.Current.Strings.ToArray());
+						dataObj.SetFileDropList(dropList);
+						dataObj.SetData("Preferred DropEffect", new MemoryStream(BitConverter.GetBytes((int)(NEClipboard.Current.IsCut == true ? DragDropEffects.Move : DragDropEffects.Copy | DragDropEffects.Link))));
+					}
+
 					Clipboard.SetDataObject(dataObj, true);
 					systemClipboard = NEClipboard.Current;
 				}
