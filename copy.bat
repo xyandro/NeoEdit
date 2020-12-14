da1473bd5bc5c301128e4bbc61fdac753797a348
rspackensen <randon_s@yahoo.com>
2020-12-14 08:51:39 -0700

    Use ready to run

--- copy.bat
+++ copy.bat
@@ -1,2 +1,2 @@
-@robocopy /mir /ndl /r:0 "C:\Dev\NeoEdit\NeoEdit\bin\Release\net5.0-windows" "C:\Users\rspackma\Documents\NeoEdit"
+@robocopy /mir /ndl /r:0 "C:\Dev\NeoEdit\Publish" "C:\Users\rspackma\Documents\NeoEdit"
 @pause
